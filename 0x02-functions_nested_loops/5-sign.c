a1e74aa0640876a22b564b0fc1ed12dcfb04fee8
cnov20 <128@holbertonschool.com>
2017-02-15 05:09:06 +0000

    Task 5 First Attempt

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,33 +1,27 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
+ * print_sign - Checks if number from input is positive, negative, or zero
+ * @n: number to be checked
  *
- * Return: Always 0.
+ * Return: returns 1, 0, -1 based on sign of input
  */
-int main(void)
-{
-	int r;
 
-	r = print_sign(98);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	r = print_sign(0);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	r = print_sign(0xff);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	r = print_sign(-1);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0);
+int print_sign(int n)
+{
+
+        if (n > 0)
+        {
+		_putchar('+');
+                return (1);
+        }
+        else if (n < 0)
+        {       _putchar('-');
+		return (-1);
+        }
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
 }
