823b1c313234fe71e08f3298dc8d3f4c5f8f6926
cnov20 <128@holbertonschool.com>
2017-02-15 04:01:06 +0000

    Task 0 - Correction #2

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 /**
+ * main - entry point of program
  * _putchar - writes the character c to stdout
- * @c: The character to print
  *
- * Return: On success 1.
+ * Return: 0 - always - success
  * On error, -1 is returned, and errno is set appropriately.
  */
 int main(void)
