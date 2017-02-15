98927971404e36f7b5a94536654f04f773abc6d3
cnov20 <128@holbertonschool.com>
2017-02-15 04:46:15 +0000

    Task 3 Attempt

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,20 +1,21 @@
 #include "holberton.h"
 
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	int r;
+#include "holberton.h"
 
-	r = _islower('H');
-	_putchar(r + '0');
-	r = _islower('o');
-	_putchar(r + '0');
-	r = _islower(108);
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0);
+/**
+ * _islower - chech character to see if an upper or lower -case
+ * @c: character to check
+ *
+ * Return: 1 - if successful - 0 - if unsucessful
+ */
+
+int _islower(int c) {
+
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else {
+		return(0);
+	}
 }
