33ffa7840dc4f81467d34ad03d9c68d86f351b72
cnov20 <128@holbertonschool.com>
2017-02-15 04:13:39 +0000

    Task 0 - comments fix

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,11 +1,11 @@
 #include "holberton.h"
+
 /**
  * main - entry point of program
- * _putchar - writes the character c to stdout
  *
  * Return: 0 - always - success
- * On error, -1 is returned, and errno is set appropriately.
  */
+
 int main(void)
 {
 	char c[] = "Holberton";
