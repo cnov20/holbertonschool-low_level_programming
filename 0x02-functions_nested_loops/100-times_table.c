ae532087e27541547480d8ced10ce4fd61d44a91
cnov20 <128@holbertonschool.com>
2017-02-15 07:54:51 +0000

    Advanced Tasks - Task 12 and Task 13

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,41 +2,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ *print_times_table - prints variable multiplaction table for given number
+ *@n: multiplication factor
+ */
+
 void print_times_table(int n)
 {
-        int row;
-        int column;
+
         int result;
+	int i;
+	int j;
+	int t1;
+	int t2;
+	int t3;
 
 	if (n > 15 || n < 0)
 	{
+		return;
 
-
-		while (i < 15 && i > 0)
+		for (i = 0; i <= n; i++)
 		{
-			j = 1;
 			_putchar('0');
-			while (j < 10)
+			for (j = 1; j <= n; j++)
 			{
 				result = i * j;
+				t1 = result % 10;
+				t2 = result / 10 % 10;
+			       	t3 = result / 100;
+				_putchar(',');
+				_putchar(' ');
+
 				if (result < 10)
 				{
 					_putchar(',');
 					_putchar(' ');
-					_putchar(' ');
-					_putchar(result + '0');
+					_putchar(t3 + '0');
+				}
+				else if (result > 100)
+				{
+					_putchar(t1 + '0');
+					_putchar(t2 + '0');
+					_putchar(t3 + '0');
 				}
 				else
 				{
 					_putchar(',');
 					_putchar(' ');
-					_putchar(result / 10 + '0');
-					_putchar(result % 10 + '0');
+					_putchar(t2 + '0');
+					_putchar(t3 + '0');
 				}
-				j++;
 			}
 			_putchar('\n');
-			i++;
 		}
         }
 }
