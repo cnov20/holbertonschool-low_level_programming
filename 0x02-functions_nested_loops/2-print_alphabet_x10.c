7c33019fc7e11ff891a33df20bce3e26f53e0fa9
cnov20 <128@holbertonschool.com>
2017-02-15 04:34:50 +0000

    Task 2 Commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,26 @@
 #include "holberton.h"
-
 /**
+ * print_alphabet_x10 - writes the alphabet to standard output
  *
- * main - entry point of program
+ * Return: void
  *
- *
- *
- * Return: 0 - always (success)
  */
 
-int main(void)
+void print_alphabet_x10(void)
 {
-	print_alphabet_x10();
-	return (0);
+	char ch[] = "abcdefghijklmnopqrstuvwxyz";
+	int i = 0;
+	int j;
+		while (i < 10)
+		{
+			j = 0;
+			while (j < 27)
+			{
+				_putchar(ch[j]);
+				j++;
+			}
 
+		   _putchar('\n');
+		   i++;
+		}
 }
