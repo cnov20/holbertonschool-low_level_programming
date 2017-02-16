61089feb0ed20b24b4618b1e53fb72878e9070a2
cnov20 <128@holbertonschool.com>
2017-02-16 00:40:04 +0000

    Task 14 - Correction

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -4,18 +4,17 @@
 
 int main(void)
 {
-       	int num1 = 1;
-	int num2 = 2;
-	int result;
-	int i;
-	int n = 50;
+       	unsigned long num1 = 0;
+	unsigned long num2 = num1 + 1;
+	unsigned long result;
+	char i;
 
-	for (i = 0; i <= n; i++)
+	for (i = 0; i <= 50; i++)
 	{
 		result = num1 + num2;
 		num1 = num2;
 		num2 = result;
-		printf("%d\n", result);
+		printf("%lu, %lu\n", num1, result);
 	}
 
 }
