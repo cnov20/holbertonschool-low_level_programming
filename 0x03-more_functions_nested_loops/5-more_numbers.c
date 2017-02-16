48bbbb1c51cbd4e050df3fe33a8408e1914d774a
cnov20 <128@holbertonschool.com>
2017-02-16 20:23:57 +0000

    Task 5 Correction

--- 0x03-more_functions_nested_loops/5-more_numbers.c
+++ 0x03-more_functions_nested_loops/5-more_numbers.c
@@ -10,22 +10,23 @@
 
 void more_numbers(void)
 {
-	int i = 0;
-	int j;
-	while (i < 15)
+	char i = 0;
+	char j;
+
+	while (i < 10)
 	{
 		j = 0;
 		while (j < 15)
 		{
 			if (j > 9)
 			{
-				_putchar(j % 10 + '0');
+				_putchar(j / 10 + '0');
 			}
-			_putchar(j + '0');
+
+			_putchar(j % 10 + '0');
 			j++;
 		}
-
-		_putchar('\n');
 		i++;
+		_putchar('\n');
 	}
 }
