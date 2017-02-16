5c6f86486e22ed998d29505fe4e8d2a85c59c4da
cnov20 <128@holbertonschool.com>
2017-02-16 02:00:47 +0000

    Task 12 - Final

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -20,25 +20,22 @@ void print_times_table(int n)
 				result = i * j;
 				if (result >= 10 && result <= 99)
 				{
-					_putchar(',');
-					_putchar(' ');
+					_putchar(','); _putchar(' ');
 					_putchar(' ');
 					_putchar(result / 10 + '0');
 					_putchar(result % 10 + '0');
 				}
 				else if (result >= 100)
 				{
-					_putchar(',');
-					_putchar(' ');
+					_putchar(','); _putchar(' ');
 					_putchar(result / 100 + '0');
 					_putchar(result / 10 % 10 + '0');
 					_putchar(result % 10 + '0');
 				}
 				else
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
+					_putchar(','); _putchar(' ');
+					_putchar(' '); _putchar(' ');
 					_putchar(result + '0');
 				}
 
