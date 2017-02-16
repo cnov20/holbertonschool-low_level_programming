cefaa8d593ec8076c2db757ef55910d7c275bcbc
cnov20 <128@holbertonschool.com>
2017-02-16 23:03:36 +0000

    Task 6 Commit

--- 0x03-more_functions_nested_loops/6-print_line.c
+++ 0x03-more_functions_nested_loops/6-print_line.c
@@ -1,1 +1,28 @@
 #include "holberton.h"
+
+/**
+ * print_line - function that prints underscore character to draw straight line
+ * @n: character that represents the underscore character
+ *
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+
+		else
+		{
+			_putchar('_');
+		}
+		i++;
+	}
+	_putchar('\n');
+}
