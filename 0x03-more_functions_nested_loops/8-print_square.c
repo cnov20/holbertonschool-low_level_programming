7767a49d249977b2ca0c10f9bca5cf36f8de0bed
cnov20 <128@holbertonschool.com>
2017-02-17 00:26:09 +0000

    Task 8 Solution

--- 0x03-more_functions_nested_loops/8-print_square.c
+++ 0x03-more_functions_nested_loops/8-print_square.c
@@ -1,1 +1,34 @@
 #include "holberton.h"
+
+/**
+ * print_square - function that prints a square followed by new line
+ * @size: variable that represents the size of the square
+ *
+ */
+
+void print_square(int size)
+{
+        int i, j;
+
+        if (size > 0)
+        {
+                i = 0;
+                while (i < size)
+                {
+                        j = 1;
+                        while (j < size)
+                        {
+				_putchar('#');
+                                j++;
+                        }
+                        _putchar('#');
+                        _putchar('\n');
+                        i++;
+                }
+        }
+        else
+        {
+                _putchar('\n');
+
+        }
+}
