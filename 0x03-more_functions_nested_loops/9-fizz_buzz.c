3da31caa2695dfd432889f29115203fff7a5ccf6
cnov20 <128@holbertonschool.com>
2017-02-17 01:21:01 +0000

    Task 9 - Fizz Buzz - Solution

--- 0x03-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,1 +1,39 @@
 #include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point of program
+ *
+ * Return: 0 - success - always
+ */
+
+int main(void)
+
+{
+	unsigned char i;
+
+	i = 1;
+	while (i <= 100)
+	{
+		if (i % 3 == 0)
+		{
+			printf("%s ", "Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("%s ", "Buzz");
+		}
+		else if (i % 15 == 0)
+		{
+			printf("%s ", "FizzBuzz");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
