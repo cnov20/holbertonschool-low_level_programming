208805b7f969265f03c9f781830d1779da1ab7f6
cnov20 <128@holbertonschool.com>
2017-02-17 00:08:27 +0000

    Task 7 -- if / else fix

--- 0x03-more_functions_nested_loops/7-print_diagonal.c
+++ 0x03-more_functions_nested_loops/7-print_diagonal.c
@@ -1,1 +1,34 @@
 #include "holberton.h"
+
+/**
+ * print_diagonal - function that prints slash character to draw diagonal line
+ * @n: character that represents the underscore character
+ *
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		i = 0;
+		while (i < n)
+		{
+			j = 0;
+			while (j < i)
+			{
+				_putchar(' ');
+				j++;
+			}
+			_putchar('\\');
+			_putchar('\n');
+			i++;
+		}
+	}
+	else
+	{
+		_putchar('\n');
+
+	}
+}
