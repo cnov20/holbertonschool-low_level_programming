e82e9460c31831e3fdede9e55188e3716f8e693b
cnov20 <128@holbertonschool.com>
2017-02-18 23:56:00 +0000

    Task 16 - int type fix

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,35 +1,34 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdint.h>
 
 /**
- * main - print first 100 fibonacci numbers
+ * main - print first 98 fibonacci numbers
  *
  * Return: 0 - always - success
  */
 
 int main(void)
 {
-        unsigned long num1 = 1;
-        unsigned long num2 = num1 + 1;
-        unsigned long result;
-        char i;
+	uint64_t num1 = 1;
+	uint64_t num2 = num1 + 1;
+	uint64_t result;
+	char i;
 
-        printf("%lu, %lu, ", num1, num2);
+	printf("%lu, %lu, ", num1, num2);
 
-        for (i = 0; i < 101; i++)
-        {
-                result = num1 + num2;
-                if (i != 47)
-                {
-                        printf("%lu, ", result);
-                }
-                else
-                {
-                        printf("%lu\n", result);
-                }
-
-                num1 = num2;
-                num2 = result;
-        }
-        return (0);
+	for (i = 0; i < 96; i++)
+	{
+		result = num1 + num2;
+		if (i != 95)
+		{
+			printf("%lu, ", result);
+		}
+		else
+		{
+			printf("%lu\n", result);
+		}
+		num1 = num2;
+		num2 = result;
+	}
+	return (0);
 }
