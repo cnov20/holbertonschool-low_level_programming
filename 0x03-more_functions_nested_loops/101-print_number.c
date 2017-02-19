54a69257dcf2bf4bac122fa0ec63a3b66d81b1d6
cnov20 <128@holbertonschool.com>
2017-02-19 01:38:18 +0000

    Fixed conditionals and operators - Output matches Task 12 output

--- 0x03-more_functions_nested_loops/101-print_number.c
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -8,34 +8,44 @@
 
 void print_number(int n)
 {
-	if (n <= -9)
+	if (n < 0)
 	{
-		_putchar(n % 10 + '0');
+		n *= -1;
+		_putchar('-');
 	}
-	else if (n <= -99)
+	if (n < 0 && n <= -9)
 	{
+		_putchar(n + '0');
+	}
+	else if (n < 0 && n <= -99)
+	{
+		_putchar(n);
+	}
+	else if (n < 0 && n <= -999)
+	{
+		_putchar(n / 100 % 10  + '0');
 		_putchar(n / 10 % 10 + '0');
 		_putchar(n % 10 + '0');
 	}
-	else if (n < -999)
+	else if (n > 0 && n <= 9)
 	{
-		_putchar(n / 100  + '0');
-		_putchar(n / 10 % 10 + '0');
+		_putchar(n + '0');
+	}
+	else if (n > 9 && n <= 99)
+	{
+		_putchar(n / 10 + '0');
 		_putchar(n % 10 + '0');
 	}
-	else if (n > 9)
+	else if (n > 99 && n <= 999)
 	{
+		_putchar(n / 100 + '0');
 		_putchar(n / 10 % 10 + '0');
-		_putchar(n % 10 + '0');
-	}
-	else if (n > 99)
-	{
-		_putchar((n / 10 % 10) + '0');
 		_putchar(n % 10 + '0');
 	}
 	else if (n > 999)
 	{
-		_putchar(n / 100  + '0');
+		_putchar(n / 1000 + '0');
+		_putchar(n / 100 % 10 + '0');
 		_putchar(n / 10 % 10 + '0');
 		_putchar(n % 10 + '0');
 	}
