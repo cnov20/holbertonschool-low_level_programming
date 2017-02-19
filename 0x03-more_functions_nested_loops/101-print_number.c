a3f02e7f1b5d18df8798fab0d8012dc3b2e39048
cnov20 <128@holbertonschool.com>
2017-02-19 01:14:14 +0000

    Fixed Betty doc style

--- 0x03-more_functions_nested_loops/101-print_number.c
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -1,35 +1,27 @@
 #include "holberton.h"
 
-/*
- * print_number - prints given integer of any size, only using _putchar
+/**
+ * print_number - prints integer of any size - using only _putchar function
  * @n: integer given, to be printed
  *
  */
 
 void print_number(int n)
 {
-
-	if (n < 0 && n > -10)
+	if (n <= -9)
 	{
-		n *= -1;
-	       	_putchar(n + '0');
+		_putchar(n % 10 + '0');
 	}
-	else if (n < -9)
+	else if (n <= -99)
 	{
-		n *= -1;
 		_putchar(n / 10 % 10 + '0');
 		_putchar(n % 10 + '0');
 	}
-	else if (n < -99)
-	{
-		n *= -1;
-		_putchar((n / 100 % 10) + '0');
-                _putchar(n / 10 % 10 + '0');
-                _putchar(n % 10 + '0');
-	}
 	else if (n < -999)
 	{
-
+		_putchar(n / 100  + '0');
+		_putchar(n / 10 % 10 + '0');
+		_putchar(n % 10 + '0');
 	}
 	else if (n > 9)
 	{
@@ -38,16 +30,14 @@ void print_number(int n)
 	}
 	else if (n > 99)
 	{
-		_putchar((n / 100) + '0');
-                _putchar((n / 10 % 10) + '0');
-                _putchar(n % 10 + '0');
+		_putchar((n / 10 % 10) + '0');
+		_putchar(n % 10 + '0');
 	}
 	else if (n > 999)
 	{
-		_putchar(n / 1000 + '0' + '0');
 		_putchar(n / 100  + '0');
-                _putchar(n / 10 % 10 + '0');
-                _putchar(n % 10 + '0');
+		_putchar(n / 10 % 10 + '0');
+		_putchar(n % 10 + '0');
 	}
 	else
 	{
