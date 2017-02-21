a6b52ab926b2e2abdd26797719f696036606d96a
cnov20 <128@holbertonschool.com>
2017-02-21 22:32:33 +0000

    Task 6 - Betty style - indentation fix

--- 0x04-pointers_arrays_strings/6-puts2.c
+++ 0x04-pointers_arrays_strings/6-puts2.c
@@ -9,16 +9,15 @@
 
 void puts2(char *str)
 {
-        int i = 0;
+	int i = 0;
 
-        while (str[i] != '\0')
-        {
-
+	while (str[i] != '\0')
+	{
 		if (i % 2 == 0)
 		{
 			_putchar(str[i]);
 		}
 		i++;
-        }
-        _putchar('\n');
+	}
+	_putchar('\n');
 }
