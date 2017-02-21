193e635828ea44338e84482a70f32bd002163d11
cnov20 <128@holbertonschool.com>
2017-02-21 22:06:05 +0000

    Fixed Betty style

--- 0x04-pointers_arrays_strings/4-print_rev.c
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -14,16 +14,15 @@ void print_rev(char *s)
 
 	i = 0;
 	while (s[i] != '\0')
-        {
-                i++;
-        }
-
+	{
+		i++;
+	}
 	i -= 1;
 	j = i;
-        while(j >= 0)
+	while (j >= 0)
 	{
 		_putchar(s[j]);
 		j--;
 	}
-	_putchar('\n');
+	_putchar ('\n');
 }
