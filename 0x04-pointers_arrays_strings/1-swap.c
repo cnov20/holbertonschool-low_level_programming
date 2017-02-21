a90d6681156a9b33e801a4921f1af2509815c8ca
cnov20 <128@holbertonschool.com>
2017-02-21 17:47:04 +0000

    Task 1 First Attempt - swap pointer values function

--- 0x04-pointers_arrays_strings/1-swap.c
+++ 0x04-pointers_arrays_strings/1-swap.c
@@ -1,1 +1,18 @@
 #include "holberton.h"
+
+
+/**
+ * swap_int - takes two integer values and swaps them
+ * @a: first ineger whose value is to be swapped
+ * @b: second integer whose value is to be swapped
+ *
+ * Return: (0)
+ *
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
