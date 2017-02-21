58dce4d48ffce5588510557700cb6423862c3089
cnov20 <128@holbertonschool.com>
2017-02-21 22:04:00 +0000

    Task 4 - Second Loop added / Reverted from calling _strlen function

--- 0x04-pointers_arrays_strings/4-print_rev.c
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -9,15 +9,21 @@
 
 void print_rev(char *s)
 {
-	int i = 0;
+	int i;
+	int j;
 
-	i = _strlen(s) - 1;
+	i = 0;
+	while (s[i] != '\0')
+        {
+                i++;
+        }
 
-	while (i > -1)
+	i -= 1;
+	j = i;
+        while(j >= 0)
 	{
-		_putchar(s[i]);
-		i--;
+		_putchar(s[j]);
+		j--;
 	}
-
 	_putchar('\n');
 }
--- 0x04-pointers_arrays_strings/5-rev_string.c
+++ 0x04-pointers_arrays_strings/5-rev_string.c
@@ -1,1 +1,21 @@
 #include "holberton.h"
+
+/**
+ * print_rev - prints string in reverse, followed by new line to std out
+ * @s: string to be printed, with new line, to std out
+ *
+ * Return: (0)
+ */
+
+void rev_string(char *s)
+{
+        int i = 0;
+
+        while (i < *s)
+        {
+		print_rev(s);
+		s++;
+        }
+
+        _putchar('\n');
+}
