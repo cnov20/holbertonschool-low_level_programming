ae0e73193b10239371cf2bfbe14c2817d8fe80d3
cnov20 <128@holbertonschool.com>
2017-02-21 18:48:15 +0000

    Task 3 - Utilize while loop and call _putchar function to write string to stdout

--- 0x04-pointers_arrays_strings/3-puts.c
+++ 0x04-pointers_arrays_strings/3-puts.c
@@ -1,1 +1,19 @@
 #include "holberton.h"
+
+/**
+ * _puts - prints string followed by new line to std out
+ * @str: string to be printed, with new line, to std out
+ *
+ * Return: (0)
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+	while (i < *str)
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
