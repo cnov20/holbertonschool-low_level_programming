52e340ed29f62bdf125a1cb8f9c641592c882522
cnov20 <128@holbertonschool.com>
2017-02-22 08:05:39 +0000

    Initial Attempt - Task 9

--- 0x04-pointers_arrays_strings/9-strcpy.c
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -12,12 +12,13 @@
 char *_strcpy(char *dest, char *src)
 {
 	int i;
-	int n;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	while (src[i] != '\0')
+	{
 		dest[i] = src[i];
-	for (i = 0 ; i < n; i++)
+		i++;
+	}
 		dest[i] = '\0';
 
-	return dest;
+	return (dest);
 }
