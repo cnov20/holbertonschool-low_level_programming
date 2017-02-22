b45b1f8259138bd1ce74cbc067bac3d8e5a2aaf6
cnov20 <128@holbertonschool.com>
2017-02-22 05:47:23 +0000

    Adding Task 8 - loop through array to print values of elements

--- 0x04-pointers_arrays_strings/8-print_array.c
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -1,1 +1,32 @@
 #include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_array - iterates through array and prints all elements
+ * @a: array to iterate through
+ * @n: number of elements in array
+ *
+ * return: 0 - success - always
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (i >= n - 1)
+		{
+			printf("%d", a[i]);
+		}
+		else
+		{
+			printf("%d, ", a[i]);
+		}
+
+			i++;
+	}
+
+	printf("\n");
+}
