7aa62ef01a11f7580c64c84cfb02b37aed4a7654
cnov20 <128@holbertonschool.com>
2017-02-22 08:00:42 +0000

    Iterator alteration

--- 0x04-pointers_arrays_strings/5-rev_string.c
+++ 0x04-pointers_arrays_strings/5-rev_string.c
@@ -4,26 +4,33 @@
  * rev_string - reverses a given string
  * @s: string to be reversed
  *
+ * Return: void
  */
 
 void rev_string(char *s)
 {
 	int i;
+
 	int j;
+
 	char converter;
 
 	j = 0;
+
 	while (s[j] != '\0')
 	{
 		j++;
 	}
+
 	j--;
+
 	i = 0;
 	while (i < j)
 	{
 		converter = s[i];
 		s[i] = s[j];
 		s[j] = converter;
+		j--;
 		i++;
 	}
 }
