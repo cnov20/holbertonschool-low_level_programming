2f030c71e640aad0d836c0d7d06e97b6159de63a
cnov20 <128@holbertonschool.com>
2017-02-24 23:31:47 +0000

    Revamped from hardcoded if /else to solution using loops

--- 0x03-more_functions_nested_loops/101-print_number.c
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -1,39 +1,51 @@
 #include "holberton.h"
 
 /**
- * print_number - prints integer of any size - using only _putchar function
- * @n: integer given, to be printed
+ * print_number - function prints(putchar only) an integer of any sign and size
+ * @n: integer to be printed
  *
+ * Return: void
  */
 
-void print_number(int n)
-{
+	void print_number(int n)
+	{
+		int b, div, length, last;
 
-	if (n >= 0)
-		n = -n;
-	else
-		_putchar('-');
-	if (n <= -1000000000)
-		_putchar(n / -1000000000 % 10 + '0');
-	if (n <= -100000000)
-		_putchar(n / -100000000 % 10 + '0');
-	if (n <= -10000000)
-		_putchar(n / -10000000 % 10 + '0');
-	if (n <= -1000000)
-		_putchar(n / -1000000 % 10 + '0');
-	if (n <= -100000)
-		_putchar(n / -10000 % 10 + '0');
-	if (n <= -10000)
-		_putchar(n / -1000 % 10  + '0');
-	if (n <= -1000)
-		_putchar(n / -1000 + '0');
-	if (n <= -100)
-		_putchar(n / -100 % 10 + '0');
-	if (n <= -10)
-		_putchar(n / -10 % 10 + '0');
-	if (n < 0)
-		_putchar((n * -1) % 10 + '0');
-	if (n == 0)
-		_putchar(n + '0');
+		last = n % 10;
 
+		if (last < 0)
+		{
+			last *= -1;
+			_putchar('-');
+		}
+
+		n /= 10;
+
+		if (n < 0)
+		{
+			n *= -1;
+		}
+
+		b = n;
+		length = 0;
+		div = 1;
+
+		while (b > 0)
+		{
+			length += 1;
+			div *= 10;
+			b /= 10;
+		}
+
+		div /= 10;
+
+		while (div >= 1)
+		{
+			_putchar(n / div + '0');
+			n %= div;
+			div /= 10;
+		}
+
+		_putchar(last + '0');
+	}
 }
