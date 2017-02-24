0dd21efd336f1d910659d8cdfd8fd20518464d36
cnov20 <128@holbertonschool.com>
2017-02-24 23:23:54 +0000

    Task 9 - Advanced - Initial Commit

---
+++ 0x05-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,50 @@
+#include "holberton.h"
+
+/**
+ * print_number - function prints an integer regardless of sign and size
+ * @n: integer to be printed
+ *
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	int b, div, length, last;
+
+	last = n % 10;
+
+	if (last < 0)
+	{
+		last *= -1;
+		_putchar('-');
+	}
+
+	n /= 10;
+
+	if (n < 0)
+	{
+		n *= -1;
+	}
+
+	b = n;
+	length = 0;
+	div = 1;
+
+	while (b > 0)
+	{
+		length += 1;
+		div *= 10;
+		b /= 10;
+	}
+
+	div /= 10;
+
+	while (div >= 1)
+	{
+		_putchar(n / div + '0');
+		n %= div;
+		div /= 10;
+	}
+
+	_putchar(last + '0');
+}
