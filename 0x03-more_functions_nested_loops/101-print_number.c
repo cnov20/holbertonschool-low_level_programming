68fc9e97232b07525b4c00dce3369a4d2975a64f
cnov20 <128@holbertonschool.com>
2017-02-24 22:00:23 +0000

    Changed if / else conditional statements

--- 0x03-more_functions_nested_loops/101-print_number.c
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -8,49 +8,32 @@
 
 void print_number(int n)
 {
-	if (n < 0)
-	{
-		n *= -1;
-		_putchar('-');
-	}
-	if (n < 0 && n <= -9)
-	{
-		_putchar(n + '0');
-	}
-	else if (n < 0 && n <= -99)
-	{
-		_putchar(n);
-	}
-	else if (n < 0 && n <= -999)
-	{
-		_putchar(n / 100 % 10  + '0');
-		_putchar(n / 10 % 10 + '0');
-		_putchar(n % 10 + '0');
-	}
-	else if (n > 0 && n <= 9)
-	{
-		_putchar(n + '0');
-	}
-	else if (n > 9 && n <= 99)
-	{
-		_putchar(n / 10 + '0');
-		_putchar(n % 10 + '0');
-	}
-	else if (n > 99 && n <= 999)
-	{
-		_putchar(n / 100 + '0');
-		_putchar(n / 10 % 10 + '0');
-		_putchar(n % 10 + '0');
-	}
-	else if (n > 999)
-	{
-		_putchar(n / 1000 + '0');
-		_putchar(n / 100 % 10 + '0');
-		_putchar(n / 10 % 10 + '0');
-		_putchar(n % 10 + '0');
-	}
+
+	if (n >= 0)
+		n = -n;
 	else
-	{
+		_putchar('-');
+	if (n <= -1000000000)
+		_putchar(n / -1000000000 % 10 + '0');
+	if (n <= -100000000)
+		_putchar(n / -100000000 % 10 + '0');
+	if (n <= -10000000)
+		_putchar(n / -10000000 % 10 + '0');
+	if (n <= -1000000)
+		_putchar(n / -1000000 % 10 + '0');
+	if (n <= -100000)
+		_putchar(n / -10000 % 10 + '0');
+	if (n <= -10000)
+		_putchar(n / -1000 % 10  + '0');
+	if (n <= -1000)
+		_putchar(n / -1000 + '0');
+	if (n <= -100)
+		_putchar(n / -100 % 10 + '0');
+	if (n <= -10)
+		_putchar(n / -10 % 10 + '0');
+	if (n < 0)
+		_putchar((n * -1) % 10 + '0');
+	if (n == 0)
 		_putchar(n + '0');
-	}
+
 }
