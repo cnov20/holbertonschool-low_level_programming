4a65e41faa674792677666419aa22f5f34217976
cnov20 <128@holbertonschool.com>
2017-02-24 02:52:00 +0000

    Task 3 - Return difference of characters

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -10,9 +10,19 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int difference;
+	int i;
 
-	difference = *s1 - *s2;
+	i = 0;
 
-	return (difference);
+	while (s1[i] == s2[i] && s1[i] != '\0')
+	{
+		i++;
+
+	}
+	if (s1[i] != s2[i])
+	{
+		return (s1[i] - s2[i]);
+	}
+
+	return (0);
 }
