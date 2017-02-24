f6cba9b058689d1c924314689ea85549f0421685
cnov20 <128@holbertonschool.com>
2017-02-24 02:17:16 +0000

    Task 3 - Solution - difference of array elements being compared

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -1,1 +1,17 @@
 #include "holberton.h"
+
+/**
+ * _strcmp- compares two strings
+ * @s1: first string to be compared
+ * @s2: second string to be compared
+ *
+ * Return: int
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int difference;
+	difference = *s1 - *s2;
+
+	return (difference);
+}
