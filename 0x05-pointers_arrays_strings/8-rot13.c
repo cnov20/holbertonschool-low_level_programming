80b045f124d81b3c8379c0a4fe1b35c5e445dfd5
cnov20 <128@holbertonschool.com>
2017-02-24 07:49:59 +0000

    Task 8 - Initial Solution

---
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * *rot13 - encode string by rotating characters 13 places
+ * @ch: character to be rotated
+ *
+ * Return: characters
+ */
+
+char *rot13(char *ch)
+{
+	int i;
+
+	for (i = 0; ch[i] != '\0'; i++)
+	{
+		if ((ch[i] >= 'a' && ch[i] <= 'm') || (ch[i] >= 'A' && ch[i] <= 'M'))
+		{
+			ch[i] += 13;
+
+		}
+		else
+		{
+			*ch -= 13;
+		}
+
+	}
+	return (ch);
+}
