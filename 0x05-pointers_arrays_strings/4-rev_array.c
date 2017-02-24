e4b8e3da3a323550e9a037b69bfac3c634dc9f1b
cnov20 <128@holbertonschool.com>
2017-02-24 04:02:48 +0000

    Task 4 - Solution - Reverse array with temp variable

--- 0x05-pointers_arrays_strings/4-rev_array.c
+++ 0x05-pointers_arrays_strings/4-rev_array.c
@@ -1,1 +1,25 @@
 #include "holberton.h"
+
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: array to be iterated through
+ * @n: number of elements in array
+ *
+ * Return: 0 - always - success
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i;
+	int j = n - 1;
+	char converter;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		converter = a[i];
+		a[i] = a[j];
+		a[j] = converter;
+		j--;
+	}
+
+}
--- 0x05-pointers_arrays_strings/5-string_toupper.c
+++ 0x05-pointers_arrays_strings/5-string_toupper.c
@@ -1,1 +1,21 @@
 #include "holberton.h"
+
+/**
+ * *string_toupper - converts lowercase letters to uppercase
+ * @char *: character array(pointer) to be iterated through
+ *
+ * Return: 0 - always - success
+ */
+
+char *string_toupper(char *)
+{
+        int i, size;
+        size = n;
+
+        for (i = size - 1; i >= 0; i--)
+        {
+                a[i] = a[n - 1];
+                a--;
+        }
+
+}
