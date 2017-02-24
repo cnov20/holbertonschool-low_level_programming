9189c3623567407c2577ba55890a58aef201b2b0
cnov20 <128@holbertonschool.com>
2017-02-24 02:22:23 +0000

    Task 3 - Fixed Betty style

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -5,12 +5,13 @@
  * @s1: first string to be compared
  * @s2: second string to be compared
  *
- * Return: int
+ * Return: int equal to difference between characters
  */
 
 int _strcmp(char *s1, char *s2)
 {
 	int difference;
+
 	difference = *s1 - *s2;
 
 	return (difference);
