e5d6d204b3059d8e1e5395fbf2b20c183558b502
cnov20 <128@holbertonschool.com>
2017-02-24 06:55:11 +0000

    Task 6 - First Attempt

--- 0x05-pointers_arrays_strings/6-cap_string.c
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -1,1 +1,46 @@
 #include "holberton.h"
+
+/**
+ * *cap_string - converts lowercase letters to uppercase
+ * @ch: character array(pointer) to be iterated through
+ *
+ * Return: words converted to uppercase
+ */
+
+char *cap_string(char *ch)
+{
+	int i;
+
+	if (*ch >= 'a' && *ch <= 'z')
+	{
+		*ch -= 32;
+	}
+
+	for (i = 0; ch[i] != '\0'; i++)
+	{
+		switch (ch[i])
+		{
+		case ' ':
+		case '\t':
+		case '\n':
+		case ',':
+		case ';':
+		case '.':
+		case '!':
+		case '?':
+		case '"':
+		case '(':
+		case ')':
+		case '{':
+		case '}':
+
+			if (ch[i + 1] >= 'a' && ch[i + 1] <= 'z')
+			{
+				ch[i + 1] -= 32;
+			}
+		}
+
+	}
+
+	return (ch);
+}
--- 0x05-pointers_arrays_strings/7-leet.c
+++ 0x05-pointers_arrays_strings/7-leet.c
@@ -1,1 +1,21 @@
 #include "holberton.h"
+
+/**
+ * *leet - encode a string into 1337
+ * @ch: character to be encoded
+ *
+ * Return: encoded characters
+ */
+
+char *leet(char *ch)
+{
+	int i;
+
+	i = 0;
+	while (ch[i] == 
+
+
+
+
+	return (ch);
+}
