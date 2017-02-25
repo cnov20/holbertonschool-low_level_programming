f3040036ba8a5a589ba70cbf32d19100a971957f
cnov20 <128@holbertonschool.com>
2017-02-25 04:38:20 +0000

    Betty fix

--- 0x03-more_functions_nested_loops/101-print_number.c
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -10,33 +10,30 @@
 void print_number(int n)
 {
 	int b, div, length, last;
-       	last = n % 10;
+
+	last = n % 10;
 
 	if (last < 0)
 	{
-       		last *= -1;
-	       	_putchar('-');
-       	}
+		last *= -1;
+		_putchar('-');
+	}
 	n /= 10;
-       	if (n < 0)
+	if (n < 0)
 	{
-       		n *= -1;
-       	}
-	b = n;
-       	length = 0;
-	div = 1;
-       	while (b > 0)
+		n *= -1;
+	}
+	b = n; length = 0; div = 1;
+	while (b > 0)
 	{
-       		length += 1;
-       		div *= 10;
-		b /= 10;
+		length += 1; div *= 10; b /= 10;
 	}
 	div /= 10;
-       	while (div >= 1)
+	while (div >= 1)
 	{
 		_putchar(n / div + '0');
-	       	n %= div;
-	       	div /= 10;
+		n %= div;
+		div /= 10;
 	}
 	_putchar(last + '0');
 }
