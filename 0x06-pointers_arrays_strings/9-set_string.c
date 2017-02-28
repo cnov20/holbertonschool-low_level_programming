24ec84c472f78efbc092b2523489a5d3fc490757
cnov20 <128@holbertonschool.com>
2017-02-28 03:36:59 +0000

    Initial attempt at Task 8

---
+++ 0x06-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * set_string - function that sets value of a pointer to a charater
+ * @s: double pointer to value of character
+ * @to: character to be pointed to
+ *
+ * Return: none
+ */
+
+
+void set_string(char **s, char *to)
+{
+
+	*s = to;
+
+}
