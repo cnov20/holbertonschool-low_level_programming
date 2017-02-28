8d2c404a10897c80bad1d2ea91b1fec1189c823e
cnov20 <128@holbertonschool.com>
2017-02-28 01:28:48 +0000

    First attempt at Task 3

---
+++ 0x06-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,50 @@
+#include "holberton.h"
+
+/**
+ * _strspn - function that gets the length of a prefix substring
+ * @s: substring one
+ * @accept: substring two
+ *
+ * Return: number of bytes shared between s and accept
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j, count;
+
+	for (i = 0; i < _strlen(s); i++)
+	{
+		for (j=0; j < _strlen(accept); j++)
+		{
+			if (s[i] != accept[j])
+			{
+				break;
+			}
+			else
+			{
+				count++;
+			}
+		}
+	}
+
+	return (count);
+}
+
+/**
+ * _strlen - takes string and calculates length
+ * @s: string taken in to find length of
+ *
+ * Return: i, string length
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
