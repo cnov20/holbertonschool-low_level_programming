3719530a537e76687e840607670c3b076fef8c20
cnov20 <128@holbertonschool.com>
2017-03-02 03:21:59 +0000

    Inital Commit - Solution

---
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -0,0 +1,47 @@
+#include "holberton.h"
+
+/**
+ * _sqrt_recursion - function that recursively returns square root of number
+ * @n: number to return square root of
+ * @x: number to test against integer (n)
+ * Return: natural square root or -1, depending on value of n
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+
+		return (helper_recursion(0, n));
+	}
+}
+
+/**
+ * * helper_recursion - helper function that determines value to be passed
+ * @x: number to test again integer (n)
+ * @n: number to return to sqrt function
+ *
+ * Return: square root of (x)
+ */
+
+int helper_recursion(int x, int n)
+{
+	if (x * x == n)
+	{
+		return (x);
+	}
+	else if (x * x > n)
+	{
+		return (-1);
+	}
+	else
+	{
+		x += 1;
+		return (helper_recursion(x, n));
+	}
+
+}
