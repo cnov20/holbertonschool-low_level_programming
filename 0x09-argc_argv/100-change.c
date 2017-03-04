dbe75290e766b08c72dbc8f2130dcd7a52d824de
cnov20 <128@holbertonschool.com>
2017-03-04 09:07:04 +0000

    Changed conditionals and added computation variables

--- 0x09-argc_argv/100-change.c
+++ 0x09-argc_argv/100-change.c
@@ -13,32 +13,57 @@
 
 int main(int argc, char **argv)
 {
-	int i;
-	int result;
-	char *endptr;
+	unsigned int penny, two_cent_piece, nickel, dime, quarter;
+	unsigned int change;
+	unsigned int num;
 	unsigned int sum;
-	unsigned int base = 10;
 
-	if (argc)
+	penny = 1;
+	two_cent_piece = 2;
+	nickel = 5;
+	dime = 10;
+	quarter = 25;
+
+	if (argc != 2)
 	{
-		for (i = 1; i < argc; i++)
-		{
-			sum = strtol(argv[i], &endptr, base);
-
-			if (*endptr)
-			{
-				printf("%s\n", "Error");
-				return (1);
-			}
-			else
-			{
-				result += sum;
-			}
-
-		}
-
-		printf("%d\n", result);
+		printf("%s\n", "Error");
+		return (1);
 	}
 
+	num = atoi(argv[1]);
+
+	if (num >= 25)
+       	{
+		change = num / quarter;
+		num %= quarter;
+		sum = change + num;
+	}
+       	else if (num >= 10 && num <= 24)
+	{
+		change = num / dime;
+		num %= dime;
+		sum = change + num;
+	}
+       	else if (num >= 5 && num <= 9)
+	{
+		change = num / nickel;
+		num %= nickel;
+		sum = change + num;
+	}
+	else if (num >= 2 && num <=4)
+	{
+		change = num / two_cent_piece;
+		num %= two_cent_piece;
+		sum = change + num;
+	}
+	else
+	{
+		change = num / penny;
+		num %= penny;
+		sum = change + num;
+	}
+
+
+	printf("%d\n", sum);
 	return (0);
 }
