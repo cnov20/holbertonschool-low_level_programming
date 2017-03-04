17d9595a2e3935b0b4c2b7ae27f1d5effaeee8df
cnov20 <128@holbertonschool.com>
2017-03-04 02:51:18 +0000

    Advanced Task - Initial commit

---
+++ 0x09-argc_argv/100-change.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - multiplies numbers passed as arguments on command line
+ * @argc: argument count
+ * @argv: argument vector (value at given index)
+ *
+ * Return: name of file, character string
+ */
+
+int main(int argc, char **argv)
+{
+	int i;
+	int result;
+	char *endptr;
+	unsigned int sum;
+	unsigned int base = 10;
+
+	if (argc)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			sum = strtol(argv[i], &endptr, base);
+
+			if (*endptr)
+			{
+				printf("%s\n", "Error");
+				return (1);
+			}
+			else
+			{
+				result += sum;
+			}
+
+		}
+
+		printf("%d\n", result);
+
+	}
+
+	return (0);
+}
