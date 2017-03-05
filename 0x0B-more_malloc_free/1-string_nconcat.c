f302658388392664d4e6923452294c34d05d51ea
cnov20 <128@holbertonschool.com>
2017-03-05 07:04:19 +0000

    Initial commit - Task 1

---
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,73 @@
+#ifndef HOLBERTON_H
+#define HOlBERTON_H
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * *string_nconcat - concatenates two strings
+ * @s1: first string to be concatenated
+ * @s2: second string to be concatenated
+ *
+ * Return: NULL if failing, pointer to concatenated string if successful
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *s;
+	unsigned int i;
+	unsigned int j;
+	unsigned int len1, len2, length;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len2++;
+	length = len1 + len2;
+
+	s = malloc(sizeof(n) * length);
+
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < len1; i++)
+	{
+       		s[i] = s1[i];
+	}
+	for (j = 0; n >= len2; j++)
+       	{
+       		s[i + j] = s2[j];
+       	}
+
+	return (s);
+}
+
+/**
+ * _strlen - takes string and calculates length
+ * @s: string taken in to find length of
+ *
+ * Return: i, string length
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
+
+#endif /* HOLBERTON_H */
