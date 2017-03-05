d06b29e22de689af48d229d2dd13d6eb8d974fef
cnov20 <128@holbertonschool.com>
2017-03-05 12:45:58 +0000

    Betty style fix and final solution

--- 0x0B-more_malloc_free/100-realloc.c
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -18,7 +18,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (ptr == NULL)
 	{
 		ptr_new = malloc(new_size);
-		return(ptr_new);
+		return (ptr_new);
 	}
 	else if (ptr != NULL && new_size == 0)
 	{
