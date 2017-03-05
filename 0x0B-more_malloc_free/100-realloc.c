3cdbe7b2ac6c52a8e43b42069a28c54e4222101c
cnov20 <128@holbertonschool.com>
2017-03-05 12:04:57 +0000

    Changed conditional order and fixed Betty style error

--- 0x0B-more_malloc_free/100-realloc.c
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -16,15 +16,15 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	ptr = malloc(old_size);
 
+	if (ptr == NULL)
+	{
+		ptr = malloc(new_size);
+	}
+
 	if (new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
-
-	}
-	else if (!ptr)
-	{
-		return malloc(new_size);
 	}
 	else if (new_size <= old_size)
 	{
@@ -40,16 +40,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		}
 
 	}
-
 	if (new_size == old_size)
 	{
 		return (ptr);
 
-	}
-
-	if (ptr == NULL)
-	{
-		ptr = malloc(new_size);
 	}
 
 	if (ptr != NULL && new_size == 0)
