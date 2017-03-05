039bf05a5fc6978570e489bb9a00592be23cbfc8
cnov20 <128@holbertonschool.com>
2017-03-05 03:45:21 +0000

    Added additional loops and iterators

--- 0x0A-malloc_free/5-argstostr.c
+++ 0x0A-malloc_free/5-argstostr.c
@@ -13,79 +13,48 @@ char *argstostr(int ac, char **av)
 {
 
 	char *new;
-	int i;
-	unsigned int total_args;
+	int i, j, k;
+	unsigned int total_args = 0;
 
 	if (ac == 0 || av == NULL)
 	{
 		return (NULL);
 	}
 
-
 	for (i = 0; i < ac; i++)
 	{
 		total_args += _strlen(av[i]) + 1;
 	}
 
-	new = malloc(total_args * sizeof(char));
+	new = malloc(total_args + 1 * sizeof(char));
 
 	if (new == NULL)
 	{
+		free(new);
 		return (NULL);
 	}
 
+	i = 0;
+	k = 0;
+	while (i < ac)
+	{
+		j = 0;
+		while (av[i][j] != '\0')
+		{
+			new[k] = av[i][j];
+			j++;
+			k++;
+		}
+		new[k] = '\n';
+		i++;
+		k++;
+
+	}
 
 	return (new);
 }
 
 
-/**
- * *str_concat - concatenates two strings
- * @s1: first string to be concatenated
- * @s2: second string to be concatenated
- *
- * Return: NULL if failing, pointer to concatenated string if successful
- */
-
-char *str_concat(char *s1, char *s2)
-{
-	char *s;
-	unsigned int i;
-	unsigned int j;
-	unsigned int len1, len2, length;
-
-	if (s1 == NULL)
-	{
-		s1 = "";
-	}
-	if (s2 == NULL)
-	{
-		s2 = "";
-	}
-
-	len1 = _strlen(s1);
-	len2 = _strlen(s2);
-	len2++;
-	length = len1 + len2;
-
-	s = malloc(sizeof(char) * length);
-
-	if (s == NULL)
-	{
-		return (NULL);
-	}
-
-	for (i = 0; i < len1; i++)
-	{
-		s[i] = s1[i];
-	}
-	for (j = 0; j < len2; j++)
-	{
-		s[i + j] = s2[j];
-	}
-
-	return (s);
-}
 
 /**
  * _strlen - takes string and calculates length
