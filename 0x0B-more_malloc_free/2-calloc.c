f1fd768420690156bffbfa83473a00051f2945d9
cnov20 <128@holbertonschool.com>
2017-03-05 08:23:39 +0000

    Initial Commit - Task 2

---
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -0,0 +1,38 @@
+#ifndef HOLBERTON_H
+#define HOlBERTON_H
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocated memory for given size, using malloc function
+ * @nmemb: number of elements of array
+ * @size: byte size of array elements
+ *
+ * Return: Pointer to allocated memory
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *arr;
+	unsigned int i;
+
+	arr = malloc(size * sizeof(nmemb));
+
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = size;
+	}
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	return (arr);
+}
+#endif /*HOLBERTON_H*/
