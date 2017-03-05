ba6ee254c88c5afe434ba8c127cb41e2b3a1f52b
cnov20 <128@holbertonschool.com>
2017-03-05 12:57:36 +0000

    Initial commit - added multiply function to file

---
+++ 0x0B-more_malloc_free/101-mul.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies numbers passed as arguments on command line
+ * @argc: argument count
+ * @argv: argument vector (value at given index)
+ *
+ * Return: name of file, character string
+ */
+
+int main(int argc, char **argv)
+{
+	int i;
+	int product = 1;
+
+	if (argc <= 2)
+	{
+		printf("%s\n", "Error");
+		exit(98);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			product *= atoi(argv[i]);
+		}
+
+	}
+
+	printf("%d\n", product);
+	return (0);
+}
