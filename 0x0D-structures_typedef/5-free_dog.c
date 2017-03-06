ad1ff09db62cc37f9a3d5808ef47b420fed55987
cnov20 <128@holbertonschool.com>
2017-03-06 09:24:17 +0000

    Added updated function prototype file and Task 5 Initial Solution commit

---
+++ 0x0D-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - function to free structure dog and its elements
+ * @d: structure passed to be free
+ *
+ * Return: none
+ */
+
+void free_dog(dog_t *d)
+{
+		free(d->name);
+		free(d->owner);
+		free(d);
+}
