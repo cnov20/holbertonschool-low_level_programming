737bb8fc528c03db48d1b64578e04a92fb34a163
cnov20 <128@holbertonschool.com>
2017-03-06 08:32:03 +0000

    Adding updated function prototype file

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -23,5 +23,7 @@ typedef struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+char *_strcpy(char *dest, char *src);
+
 
 #endif /*DOG_H*/
