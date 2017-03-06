a9bf99566650c576661c979917dd8a9f6915c25f
cnov20 <128@holbertonschool.com>
2017-03-06 08:31:16 +0000

    Added new malloc calls for copy of name and owner

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -13,16 +13,45 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-
 	struct dog *new;
 
 	new = malloc(sizeof(struct dog));
 
 	if (new == NULL)
 		return (NULL);
-	new->name = name;
-	new->age = age;
-	new->owner = owner;
-	return (new);
 
+	new->name = malloc(sizeof(char));
+
+	_strcpy(new->name, name);
+
+	new->owner = malloc(sizeof(char));
+
+	_strcpy(new->owner, owner);
+
+	new->age = age;
+
+	return (new);
+}
+
+/**
+ * _strcpy - copies a string
+ *@dest: points to buffer
+ *@src: points to string
+ *
+ * Return: the pointer to dest
+ *
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
 }
