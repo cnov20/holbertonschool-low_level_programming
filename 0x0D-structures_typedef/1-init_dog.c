e5acc4b766a1835974e901fd0c692b8669981d02
cnov20 <128@holbertonschool.com>
2017-03-06 05:11:58 +0000

    Structure declaration added before if statement

--- 0x0D-structures_typedef/1-init_dog.c
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -1,6 +1,6 @@
+#include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "dog.h"
 
 /**
  * init_dog - structure that declares variable with info about a dog
@@ -14,8 +14,11 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	struct dog;
+
 	if (d == NULL)
 		return;
+
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
