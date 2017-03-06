956f77dc91555b115513e4143193094e862da7a9
cnov20 <128@holbertonschool.com>
2017-03-06 08:35:34 +0000

    Changed size type of malloc call

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -15,7 +15,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	struct dog *new;
 
-	new = malloc(sizeof(struct dog_t));
+	new = malloc(sizeof(dog_t));
 
 	if (new == NULL)
 		return (NULL);
