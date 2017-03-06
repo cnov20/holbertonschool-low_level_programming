91aefbab811d44368e5add96a4122c0514bebc35
cnov20 <128@holbertonschool.com>
2017-03-06 05:08:51 +0000

    Task 1 Solution

---
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * init_dog - structure that declares variable with info about a dog
+ * @d: pointer variable initalzed as type 'struct dog'
+ * @name: name of dog
+ * @owner: name of dog's owner
+ * @age: age of dog
+ *
+ * Return: none
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		return;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
