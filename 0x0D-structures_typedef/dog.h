d595b5227ac1ebb73be0f0c731829f5051d13ed8
cnov20 <128@holbertonschool.com>
2017-03-06 04:20:33 +0000

    Task 0 - Betty style correction

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -1,6 +1,15 @@
-#include <stdio.h>
 #ifndef DOG_H
 #define DOG_H
+#include <stdio.h>
+
+/**
+ * struct dog - structure that declares variable with info about a dog
+ * @name: name of dog
+ * @owner: name of dog's owner
+ * @age: age of dog
+ *
+ * Return: none
+ */
 
 struct dog
 {
