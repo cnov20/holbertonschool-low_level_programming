e809e4e05d1f3d33d3b56da40ab19eaf8418f584
cnov20 <128@holbertonschool.com>
2017-03-06 06:52:39 +0000

    Initial commit - Task 2 solution

---
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -0,0 +1,38 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_dog - structure that declares variable with info about a dog
+ * @d: pointer variable initalzed as type 'struct dog'
+ *
+ * Return: none
+ */
+
+void print_dog(struct dog *d)
+{
+
+	if (d)
+	{
+		if (d->name != NULL)
+		{
+			printf("Name: %s\n", d->name);
+		}
+		else
+		{
+			printf("Name: (nil)");
+		}
+
+		printf("Age: %f\n", d->age);
+
+		if (d->owner != NULL)
+		{
+			printf("Owner: %s\n", d->owner);
+		}
+		else
+		{
+			printf("Ownder: (nil)");
+		}
+
+	}
+}
