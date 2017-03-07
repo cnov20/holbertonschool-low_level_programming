edc40ecb740050dd0c51711a33bba2b39f90a325
cnov20 <128@holbertonschool.com>
2017-03-07 11:15:19 +0000

    Added _H to header file include guards

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -1,5 +1,5 @@
-#ifndef VARIADIC_FUNCTIONS
-#define VARIADIC_FUNCTIONS
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
@@ -8,4 +8,4 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-#endif /*VARIADIC_FUNCTIONS*/
+#endif /*VARIADIC_FUNCTIONS_H*/
