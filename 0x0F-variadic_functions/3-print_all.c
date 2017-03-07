d179b6171958ef6719e06cdc56088924fd5e7d36
cnov20 <128@holbertonschool.com>
2017-03-07 09:54:53 +0000

    Setup switch statement to handle different types of input

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -9,11 +9,41 @@
 
 void print_all(const char * const format, ...)
 {
-	if (s == NULL)
+	/* declaring variable of nums passed to function, with variable type*/
+	va_list all_args;
+
+	const char *p;
+	char *s;
+	char c;
+	int i;
+	float f;
+
+	/*initializing all_args  so that it will store values passed in*/
+	va_start(all_args, format);
+
+	p = format;
+	while (*p != '\0')
 	{
-		printf("(nil)");
+		switch (*p++)
+		{
+		case 's':
+			s = va_arg(all_args, char *);
+			printf("%s", s);
+			break;
+		case 'i':
+			i = va_arg(all_args, int);
+			printf("%d", i);
+			break;
+		case 'f':
+			f = va_arg(all_args, double);
+			printf("%f", f);
+			break;
+		case 'c':
+			c = va_arg(all_args, int);
+			putchar(c);
+			break;
+		}
 	}
-
-
+	va_end(all_args);
 	printf("\n");
 }
