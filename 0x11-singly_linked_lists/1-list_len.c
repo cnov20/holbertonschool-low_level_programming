377a1098c0d7f60d69cd825eb50d4b229947cd63
cnov20 <128@holbertonschool.com>
2017-03-09 05:29:17 +0000

    Initial commit - Task 1

---
+++ 0x11-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * list_len - returns number of elemnents in a linked list
+ * @h: pointer to head of linked list
+ *
+ * Return: number of nodes in linked list
+ */
+
+size_t list_len(const list_t *h)
+{
+
+	unsigned int length = 0;
+
+        while (h != NULL)
+        {
+                h = h->next;
+                length++;
+        }
+
+        return (length);
+
+}
