2b9a1794a327235a4327f19a87175a4b8e049804
cnov20 <128@holbertonschool.com>
2017-03-09 05:24:45 +0000

    Initial Commit - README and Task 0

---
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * print_list - prints all lements of a list_t structured list
+ * @h: pointer to head of linked list
+ *
+ * Return: number of nodes in linked list
+ */
+
+size_t print_list(const list_t *h)
+{
+	unsigned int length = 0;
+
+	while (h != NULL)
+	{
+		printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		length++;
+	}
+
+	return (length);
+}
---
+++ 0x11-singly_linked_lists/README.md
@@ -0,0 +1,6 @@
+What you should learn from this project
+
+At the end of this project you are expected to be able to explain to anyone, without the help of Google:
+
+When and why using linked lists vs arrays
+How to build and use linked lists
