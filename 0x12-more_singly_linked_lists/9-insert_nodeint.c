3fd7a049a5800a76ae47e5868ad7b5272c118733
cnov20 <128@holbertonschool.com>
2017-03-10 20:21:57 +0000

    Changed return value to current_node

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -35,5 +35,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node->next = current_node->next;
 	current_node->next = new_node;
 
-	return (*head);
+	return (new_node);
 }
