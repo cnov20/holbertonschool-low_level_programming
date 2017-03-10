55a6a5f193ae6deae43432558c80aba8b9f80548
cnov20 <128@holbertonschool.com>
2017-03-10 23:47:49 +0000

    Formatting fix - Betty style - comments and indentation

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -2,7 +2,7 @@
 /**
  * delete_nodeint_at_index - deletes a node at a given position
  * @head: pointer to pointer to head of linked list
- * @idx: index of the list where the new node should be deleted
+ * @index: index of the list where the new node should be deleted
  *
  * Return: (1) on SUCCESS - (-1) on FAILURE
  */
@@ -27,12 +27,12 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 
 		deleted_node = deleted_node->next;
-		return(-1);
+		return (-1);
 		i++;
-	 }
+	}
 
 	current_node = deleted_node->next;
-       	deleted_node->next = current_node->next;
+	deleted_node->next = current_node->next;
 	free(current_node);
 	return (1);
 }
