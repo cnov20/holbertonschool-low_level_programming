775b7d0575de323898e48facc8bb77c38b37dacc
cnov20 <128@holbertonschool.com>
2017-03-10 21:19:27 +0000

    Added conditional to if statement - if *head pointer is NULL

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node;
 	listint_t *current_node;
 
-	if (head == NULL)
+	if (head == NULL || *head == NULL)
 	{
 		return (NULL);
 	}
