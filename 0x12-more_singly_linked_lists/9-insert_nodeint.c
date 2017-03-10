9a09b80b3fe3bf85c7b5abe091e17f706fed2924
cnov20 <128@holbertonschool.com>
2017-03-10 21:15:46 +0000

    Refactored code, moved variable assignments outside while loop and changed final return value

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,6 +11,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 
 	listint_t *new_node;
+	listint_t *current_node;
 
 	if (head == NULL)
 	{
@@ -23,20 +24,19 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		new_node->next = *head;
-		*head = new_node;
 		return (new_node);
 	}
 
-	listint_t *current_node = *head;
+	current_node = *head;
 
 	while (idx - 1 > 0)
 	{
 		current_node = current_node->next;
-		new_node->next = current_node->next;
 		idx--;
-		return (new_node);
 	}
 
-			current_node->next = new_node;
-			return (NULL);
+	new_node->next = current_node->next;
+	current_node->next = new_node;
+
+	return (*head);
 }
