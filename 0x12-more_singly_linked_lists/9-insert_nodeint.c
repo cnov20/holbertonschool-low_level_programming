f194ac326449cb5f342039f286bcb78c2e3a6f14
cnov20 <128@holbertonschool.com>
2017-03-10 21:54:02 +0000

    Changed return to NULL if head is NULL

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,7 +20,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (head == NULL)
 	{
 		*head = new_node;
-		return (*head);
+		return (NULL);
 	}
 	else if (idx == 0)
 	{
