777a12edf55538c4394634aa74816e8bd06a9613
cnov20 <128@holbertonschool.com>
2017-03-10 20:15:46 +0000

    Changed while loop conditional to idx - 1

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,7 +26,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	listint_t *current_node = *head;
 
-	while (idx > 0)
+	while (idx - 1 > 0)
 	{
 		current_node = current_node->next;
 		idx--;
