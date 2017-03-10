8f08f2dd06ede61e87a9fab62eba15d931d368fe
cnov20 <128@holbertonschool.com>
2017-03-10 18:19:36 +0000

    Added free call for (head) pointer

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -9,13 +9,14 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-
 	while (*head != NULL)
 	{
 		temp = *head;
 		*head = temp->next;
 		free(temp);
+
 	}
 
-	head = NULL;
+		head = NULL;
+			free(head);
 }
