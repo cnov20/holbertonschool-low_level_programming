962b5fce78525589bbdcb477d447a38e0f6d6dcc
cnov20 <128@holbertonschool.com>
2017-03-10 23:45:44 +0000

    Changed if statement conditional

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -15,7 +15,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	deleted_node = *head;
 
-	if (index == 1)
+	if (index == 0)
 	{
 		*head = deleted_node->next;
 		free(deleted_node);
