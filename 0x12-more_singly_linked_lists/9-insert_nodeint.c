ecb5168e2c324d5b8a664efd7e2199da27ad2e67
cnov20 <128@holbertonschool.com>
2017-03-10 20:46:22 +0000

    Re factored while loops and return statements

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,18 +9,21 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node = malloc(sizeof(listint_t));
 
-	new_node->n = n;
+	listint_t *new_node;
 
 	if (head == NULL)
 	{
 		return (NULL);
 	}
 
+	new_node = malloc(sizeof(listint_t));
+	new_node->n = n;
+
 	if (idx == 0)
 	{
 		new_node->next = *head;
+		*head = new_node;
 		return (new_node);
 	}
 
@@ -31,8 +34,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		current_node = current_node->next;
 		new_node->next = current_node->next;
 		idx--;
+		return (new_node);
 	}
 
 			current_node->next = new_node;
-			return (new_node);
+			return (NULL);
 }
