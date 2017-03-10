041d75786a2801fe9432a8e023725f25cfb8f4ad
cnov20 <128@holbertonschool.com>
2017-03-10 21:35:11 +0000

    Changed return and assignment if head is NULL

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,13 +13,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node;
 	listint_t *current_node;
 
-	if (head == NULL && *head == NULL)
-	{
-		return (NULL);
-	}
-
 	new_node = malloc(sizeof(listint_t));
 	new_node->n = n;
+
+	if (head == NULL && *head == NULL)
+	{
+		*head = new_node;
+		return (*head);
+	}
 
 	if (idx == 0)
 	{
