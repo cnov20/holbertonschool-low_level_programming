487fb18897b962adf7f425875ac4dd7a55e001b6
cnov20 <128@holbertonschool.com>
2017-03-10 22:32:46 +0000

    Removed redundant NULL conditional

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,11 +17,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node = malloc(sizeof(listint_t));
 	new_node->n = n;
 
-	if (head == NULL)
-	{
-		*head = new_node;
-		return (NULL);
-	}
 	if (idx == 0)
 	{
 		new_node->next = *head;
