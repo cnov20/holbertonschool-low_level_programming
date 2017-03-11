7edbc83ba1577dce2bf4a34ac0c280b26556a915
cnov20 <128@holbertonschool.com>
2017-03-11 16:30:07 +0000

    First solution attempt - one output case incorrect

---
+++ 0x13-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * clear_bit - sets the value of a bit to 0 at a given index
+ * @n: pointer to number given
+ * @index: index of bit to be set
+ *
+ * Return: 1 if success or -1 upon failure
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+
+	if (index > sizeof(n) * 8 - 1)
+		return (-1);
+
+	*n &= 0 >> index;
+	return (1);
+
+}
