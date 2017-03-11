70cb00bc85b6236ffcd142b0b267caf845f9b31e
cnov20 <128@holbertonschool.com>
2017-03-11 15:00:41 +0000

    Refactored while loop and added size of for portability- changed iterator to regular integer

--- 0x13-bit_manipulation/1-print_binary.c
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -1,34 +1,32 @@
 #include "holberton.h"
 /**
- * print_binary - coverts an integert to binary and prints it
+ * print_binary - converts an integer to binary and prints it
  * @n: integer to be converted to binary and printe to std out
  *
  * Return: binary number converted to unsigned int
  */
 void print_binary(unsigned long int n)
 {
+	int i;
+	bool flag = false;
 
-	unsigned long int itob;
-	unsigned int i = 0;
-
-	itob = n;
-
-	if (itob == 0)
+	if (n == 0)
 	{
 		_putchar('0');
+		return;
 	}
-	while (itob > 0)
-	{
-		if ((itob & n) == 0)
-		{
-			_putchar('0');
-		}
-		else
-		{
-			_putchar('1');
-		}
 
-		itob >>= 1;
-		i--;
+	for(i = sizeof(n) * 8 - 1; i >= 0; i--)
+	{
+	       	if ((n >> i) & 1)
+	       	{
+	       		_putchar('1');
+			flag = true;
+	       	}
+	       	else if (flag == true)
+	       	{
+	       		_putchar('0');
+	       	}
+
 	}
 }
