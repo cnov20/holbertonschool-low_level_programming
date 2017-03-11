253d4528794559362c91c3506ea90e1ee645f4ce
cnov20 <128@holbertonschool.com>
2017-03-11 04:55:16 +0000

    Refactored code - Added second, nested while loop and conditional, utilized two temp pointers

--- 0x12-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x12-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
  * print_listint_safe - prints all the elements of a list
- * @h: pointer to head of linked list
+ * @head: pointer to head of linked list
  *
  * Return: number of nodes in linked list
  */
@@ -9,13 +9,29 @@
 size_t print_listint_safe(const listint_t *head)
 {
 	unsigned int length = 0;
+	unsigned int j;
 	const listint_t *temp = head;
+	const listint_t *temp2;
 
-	while (head != NULL)
+
+
+	while (temp != NULL)
 	{
-		printf("[%p] %d\n", (void *)temp->next, temp->n);
+		printf("[%p] %d\n", (void *)temp, temp->n);
 		temp = temp->next;
+		temp2 = head;
 		length++;
+		for (j = 0; j < length; j++)
+		{
+			if (temp == temp2)
+			{
+				printf("-> [%p] %d\n", (void *)temp, temp2->n);
+				exit(98);
+				return (length);
+			}
+
+			temp2 = temp2->next;
+		}
 	}
 
 	return (length);
