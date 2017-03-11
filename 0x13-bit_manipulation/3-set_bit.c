415f036b032b0ad2b89c5d9994dc3560706d5d24
cnov20 <128@holbertonschool.com>
2017-03-11 16:23:14 +0000

    Removed bitwise operator assignment from if statement due to betty style

--- 0x13-bit_manipulation/3-set_bit.c
+++ 0x13-bit_manipulation/3-set_bit.c
@@ -4,7 +4,7 @@
  * @n: pointer to number given
  * @index: index of bit to be set
  *
- * Return: value of bit at given index or -1 upon failure
+ * Return: 1 if success or -1 upon failure
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
@@ -12,11 +12,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 	if (index > sizeof(n) * 8 - 1)
 		return (-1);
 
-	if (*n |= 1 << index)
-	{
-		return (1);
-	}
-
-	return (0);
+	*n |= 1 << index;
+	return (1);
 
 }
