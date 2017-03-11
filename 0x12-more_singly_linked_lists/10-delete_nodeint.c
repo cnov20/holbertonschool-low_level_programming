3e82a53e733e6243db7a4c6bac551c7d6ce0bf80
cnov20 <128@holbertonschool.com>
2017-03-11 00:13:06 +0000

    Added OR conditional for head equals NULL

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,7 +13,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *current_node;
 	unsigned int i;
 
-	if (head == NULL && *head == NULL)
+	if (head == NULL || *head == NULL)
 		return (-1);
 
 	deleted_node = *head;
