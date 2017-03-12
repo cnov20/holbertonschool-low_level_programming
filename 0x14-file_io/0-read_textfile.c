89feebe1b4d81f6c319744e05a634a555dbf1af7
cnov20 <128@holbertonschool.com>
2017-03-12 23:55:46 +0000

    Task 0 - first solution attempt

---
+++ 0x14-file_io/0-read_textfile.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+
+/**
+ * read_textfile - function reads a text file and prints it to POSIX std output
+ * @filename: text file to be read and printed
+ * @letters: number of letters to be read and printed
+ *
+ * Return: Actual number of letters successfully read and printed
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+	int fd_open, fd_read, fd_write;
+	char *buffer;
+	char ch;
+
+	if (filename == NULL)
+		return (0);
+
+	/* malloc space for file to be read and printed to std out */
+
+	buffer = malloc(sizeof(ch) * (letters + 1));
+
+	/* open */
+
+	fd_open = open(filename, O_RDONLY);
+
+	/* read */
+
+	fd_read = read(fd_open, buffer, letters);
+
+	/* write */
+
+	fd_write = write(STDOUT_FILENO, buffer, letters);
+
+	if (fd_write == -1)
+		return (0);
+
+	close(fd_write);
+	return (fd_read);
+}
