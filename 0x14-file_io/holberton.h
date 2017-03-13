86cfe3a659927bec8e12670ccb8ab7e31b6c93da
cnov20 <128@holbertonschool.com>
2017-03-13 05:33:43 +0000

    Added helper function prototypes to header file

--- 0x14-file_io/holberton.h
+++ 0x14-file_io/holberton.h
@@ -12,5 +12,9 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void usage_failure(void);
+void read_failure(char *fd);
+void write_failure(char *fd);
+void close_failure(int fd);
 
 #endif /* HOLBERTON_H */
