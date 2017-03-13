e738076128f5a94dfc103d1240cb118787eac4d9
cnov20 <128@holbertonschool.com>
2017-03-13 05:23:07 +0000

    Added parameters to helper functions

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -14,10 +14,16 @@ int main(int ac, char **av)
 	int fd_from, fd_to, fd_close;
 	int length;
 	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
-	char *buffer[1204];
+	char *buffer;
 
 	if (filename == NULL)
 		return (-1);
+
+	if (ac != 3)
+		usage_failure();
+
+	if(av[1] == NULL)
+		read_failure(
 
 	/* allocate space for file to be read and printed to std out*/
 	buffer = malloc(sizeof(char) * BUFF_SIZE);
@@ -26,20 +32,19 @@ int main(int ac, char **av)
        	fd_from = open(av[1], O_RDONLY);
 
 	if (fd_from == -1)
-		return (-1);
-
-	fd_to = open(av[2], O_CREAT | O_RDWR | O_TRUNC, mode, mode);
 
 	/* write */
-	if (text_content != NULL)
+	fd_to = open(av[2], O_CREAT | O_RDWR | O_TRUNC, mode);
+
+	if (fd_from != NULL)
 	{
 		length = 0;
-		while (text_content[length] != '\0')
+		while ([length] != '\0')
 		{
 			length++;
 		}
 
-		fd_write = write(fd_from, text_content, length);
+		fd_write = write(fd_to, buffer, length);
 	}
 
 	if (fd_write == -1)
@@ -75,7 +80,7 @@ void usage_failure(void)
  * Return: nothing - void
  */
 
-void read_failure(void)
+void read_failure(char *fd)
 {
 	if (fd_from == -1)
 	{
@@ -91,7 +96,7 @@ void read_failure(void)
  * Return: nothing - void
  */
 
-void write_failure(void)
+void write_failure(char *fd)
 {
 	if (fd_to == -1)
 	{
@@ -106,7 +111,7 @@ void write_failure(void)
  * Return: nothing - void
  */
 
-void close_failure(void)
+void close_failure(int fd)
 {
 	if (fd_close == -1)
 	{
