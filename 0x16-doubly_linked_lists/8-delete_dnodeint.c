f86ea50aa04b9297933cfeff6effe5b7dd8051fb
cnov20 <128@holbertonschool.com>
2017-03-20 21:28:46 +0000

    Removed free(temp) from while loop and changed to temp->next just prior to final return

--- 0x16-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x16-doubly_linked_lists/8-delete_dnodeint.c
@@ -30,7 +30,6 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	{
 		if (temp == NULL || temp->next == NULL)
 		{
-			free(temp);
 			return (-1);
 		}
 
@@ -40,7 +39,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 
 	temp->prev->next = temp->next;
-	temp->next->prev = temp->prev;
+	temp->next->prev = temp->next;
 
 	free(temp);
 	return (1);
