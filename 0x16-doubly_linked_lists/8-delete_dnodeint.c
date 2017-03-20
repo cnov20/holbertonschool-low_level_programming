811072275e3b4ea44f6cb7db113cda36ff4c680c
cnov20 <128@holbertonschool.com>
2017-03-20 21:02:50 +0000

    Moved temp - NUll conditional into if statement

--- 0x16-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x16-doubly_linked_lists/8-delete_dnodeint.c
@@ -18,12 +18,14 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	temp = *head;
 
-	if (temp == NULL)
-		return (-1);
 
 	if (index == 0)
 	{
 		*head = temp->next;
+
+		if (temp == NULL)
+		return (-1);
+
 		free(temp);
 		return (1);
 	}
