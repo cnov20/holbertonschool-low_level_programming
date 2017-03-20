c15d8d929c5108442b21bddcd854ce974e7ee425
cnov20 <128@holbertonschool.com>
2017-03-20 09:44:22 +0000

    Set own if conditional for if temp is NULLto account for singled item list

--- 0x16-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x16-doubly_linked_lists/3-add_dnodeint_end.c
@@ -8,18 +8,19 @@
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *new_node;
-	dlistint_t *temp;
+	dlistint_t *new_node, *temp;
 
 	temp = *head;
 
-	new_node = malloc(sizeof(dlistint_t));
-
-	if (new_node == NULL || temp == NULL)
-	{
-		*head = new_node;
+	if (temp == NULL)
+	{	*head = new_node;
 		return (NULL);
 	}
+
+	new_node = malloc(sizeof(dlistint_t));
+
+	if (new_node == NULL)
+		return (NULL);
 
 	new_node->n = n;
 	new_node->prev = NULL;
