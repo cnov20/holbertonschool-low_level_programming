508caf85f6ae1fc59dae45292ffda067fc336d65
cnov20 <128@holbertonschool.com>
2017-03-20 09:34:30 +0000

    Fixed formatting - tab (line 21) and reduced to 40 lines(per function limit) - to match Betty specifications

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,7 +10,6 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-
 	dlistint_t *temp;
 	dlistint_t *new_node;
 	unsigned int node_number;
@@ -18,7 +17,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (h == NULL)
 		return (NULL);
 
-       	new_node = malloc(sizeof(dlistint_t));
+	new_node = malloc(sizeof(dlistint_t));
 
 	if (new_node == NULL)
 		return (NULL);
