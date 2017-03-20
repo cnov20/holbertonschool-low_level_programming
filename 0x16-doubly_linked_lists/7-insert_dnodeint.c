fed3bf565b3305085c391191c6fbbd918d7fcdc8
cnov20 <128@holbertonschool.com>
2017-03-20 05:18:02 +0000

    Re-factor - move NULL conditional handling above variable assignment

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,15 +15,15 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new_node;
 	unsigned int node_number;
 
-	new_node = malloc(sizeof(dlistint_t));
-	new_node->n = n;
-	new_node->next = NULL;
-	new_node->prev = NULL;
-
 	if (h == NULL)
 	{
 		return (NULL);
 	}
+
+       	new_node = malloc(sizeof(dlistint_t));
+	new_node->n = n;
+	new_node->next = NULL;
+	new_node->prev = NULL;
 
 	if (idx == 0)
 	{
