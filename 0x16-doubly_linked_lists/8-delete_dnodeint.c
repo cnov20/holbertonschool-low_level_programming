6e795a1d7d24d3f26d672a8f7a2abcc061d70c60
cnov20 <128@holbertonschool.com>
2017-03-20 06:00:56 +0000

    Task 8 - Delete node at given index - Initial

---
+++ 0x16-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+/**
+ * delete_dnodeint_at_index - deltes node at given position of list
+ * @head: pointer to pointer to first node of list
+ * @idx: index of node - location within list
+ *
+ * Return: 1 - SUCCESS or -1 - FAILURE
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+
+	dlistint_t *temp;
+/*	d_listint_t *node_to_delete; */
+	unsigned int node_number;
+
+	if (head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		return (-1);
+	}
+
+	temp = *head;
+	node_number = 0;
+
+	while (temp != NULL)
+	{
+		if (index == node_number + 1)
+		{
+			*head = temp->next;
+			temp->next = *head;
+			temp->prev = temp;
+			return (1);
+		}
+
+		temp = temp->next;
+		node_number++;
+	}
+
+	return (-1);
+}
