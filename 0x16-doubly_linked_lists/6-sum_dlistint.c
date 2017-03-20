035663660f75a2e513a9cc86871673aedadd1b47
cnov20 <128@holbertonschool.com>
2017-03-20 04:06:21 +0000

    Task 6 - Sum data in doubly linked list - Initial

---
+++ 0x16-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * sum_dlistint - adds and then returns sum of all data of doubly linked list
+ * @head: pointer to first node of list
+ *
+ * Return: sum of list data or 0 - if list is empty
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	unsigned int sum = 0;
+	dlistint_t *temp = head;
+
+	if (head == NULL)
+		return (0);
+
+	while (temp != NULL)
+	{
+		sum += temp->n;
+		temp = temp->next;
+	}
+
+	return (sum);
+}
