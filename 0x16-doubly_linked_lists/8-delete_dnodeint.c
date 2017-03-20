a2d5468db9ba3f2757b9bdd842180e496a125cb9
cnov20 <128@holbertonschool.com>
2017-03-20 21:31:25 +0000

    Set temp's previous pointer equal to NULL

--- 0x16-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x16-doubly_linked_lists/8-delete_dnodeint.c
@@ -39,7 +39,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 
 	temp->prev->next = temp->next;
-	temp->next->prev = temp->next;
+	temp->next->prev = NULL;
 
 	free(temp);
 	return (1);
