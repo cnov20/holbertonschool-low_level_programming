bb4ba51b963949ef670d79d8fed7d5ce54684842
cnov20 <128@holbertonschool.com>
2017-03-20 02:25:06 +0000

    Refactored code, changed while loop body and conditional

---
+++ 0x16-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+/**
+ * *add_dnodeint_end - adds a new node at the end of a doubly linked list
+ * @head: pointer to head pointer to first node of list
+ * @n: data member of node
+ *
+ * Return: address of new node or NULL upon failure
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+	dlistint_t *temp;
+
+	temp = *head;
+
+	new_node = malloc(sizeof(dlistint_t));
+
+	if (new_node == NULL || temp == NULL)
+	{
+		*head = new_node;
+		return (NULL);
+	}
+
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = NULL;
+
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+		new_node->prev = temp;
+	}
+
+	temp->next = new_node;
+
+	return (new_node);
+
+}
