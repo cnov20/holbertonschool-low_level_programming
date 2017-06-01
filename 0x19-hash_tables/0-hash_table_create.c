fad75a97cd62b7886e254f03b6265a77cc43383c
cnov20 <128@holbertonschool.com>
2017-06-01 09:49:40 +0000

    Task 0 - create hash table - Initial

---
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of hash table array
+ *
+ * Return - pointer to newly created hash table or NULL
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+
+	hash_table_t *hash_table;
+/*	unsigned int i; */
+
+	hash_table = malloc(sizeof(hash_table_t));
+
+	if (hash_table == NULL)
+		return NULL;
+
+	hash_table->array = malloc(sizeof(hash_table_t) * size);
+
+	if(hash_table->array == NULL)
+		return NULL;
+
+
+
+	return (hash_table);
+}
