e7fb364e4de1df67d4208e950ff48e1d932101f6
cnov20 <128@holbertonschool.com>
2017-06-01 23:36:09 +0000

    Corrected strlen value

--- 0x19-hash_tables/4-hash_table_get.c
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -14,7 +14,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int value;
 	hash_node_t *temp_ptr;
 
-	if (!ht || !ht->array || !key || strlen(key) == NULL)
+	if (!ht || !ht->array || !key || strlen(key) == 0)
 		return (NULL);
 
 	value = key_index((const unsigned char *)key, ht->size);
