dec6750ebb64b07c56c0b893a58109eb8e1c70ef
cnov20 <128@holbertonschool.com>
2017-06-01 17:17:23 +0000

    Removed second strlen call and fixed betty spacing

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -37,12 +37,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	ht->array[index] = new_node;
 	new_node->key = key_copy;
-        new_node->value = value_copy;
+	new_node->value = value_copy;
 	new_node->next = NULL;
 
-	if (strlen(key_copy) == 0)
-		return (0);
-
 	return (1);
-
 }
