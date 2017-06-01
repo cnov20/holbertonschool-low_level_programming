f265e4a6a95ef5c7a71ebc8ed7d7df7501a8a262
cnov20 <128@holbertonschool.com>
2017-06-01 16:40:30 +0000

    Added check for hash table array being NULL

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -16,7 +16,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	char *value_copy;
 	hash_node_t *new_node;
 
-	if (!ht || !key || !value)
+	if (!ht || !ht->array || !key || !value)
 	{
 		return (0);
 	}
