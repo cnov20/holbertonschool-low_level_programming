e12e9b0330ae00889ca72ac1ab33296b7f5534c3
cnov20 <128@holbertonschool.com>
2017-06-01 10:22:18 +0000

    Moved hash table size set back to end of function

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -31,11 +31,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return NULL;
 	}
 
-	hash_table->size = size;
 	for(i = 0; i < size; i++)
 	{
 		hash_table->array[i] = NULL;
 	}
 
+
+	hash_table->size = size;
 	return (hash_table);
 }
