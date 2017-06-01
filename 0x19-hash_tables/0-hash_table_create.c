7bb2297db19570774756fdae4bbb2716150574c7
cnov20 <128@holbertonschool.com>
2017-06-01 10:00:52 +0000

    Fixed for loop to set pointers at each array index to NULL

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -18,7 +18,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (hash_table == NULL)
 		return NULL;
 
-	hash_table->array = malloc(sizeof(hash_node_t) * size);
+	hash_table->array = malloc(sizeof(hash_node_t*) * size);
 
 	if(hash_table->array == NULL)
 		return NULL;
