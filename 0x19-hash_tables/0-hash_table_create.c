91e96810da621e6128d0ac50d9d7eae224c290e9
cnov20 <128@holbertonschool.com>
2017-06-01 09:55:42 +0000

    Changed malloc to size of hash node for arrary elements

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -18,10 +18,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (hash_table == NULL)
 		return NULL;
 
-	hash_table->array = malloc(sizeof(hash_table_t) * size);
+	hash_table->array = malloc(sizeof(hash_node_t) * size);
 
 	if(hash_table->array == NULL)
 		return NULL;
+
 	for(i = 0; i < size; i++)
 	{
 		hash_table->array = NULL;
