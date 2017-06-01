c38728b316554c333a25ca15a5770559a666dc88
cnov20 <128@holbertonschool.com>
2017-06-01 17:41:23 +0000

    Added Task 4 - Retrieves a value associated with a given key

---
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value associated with a given key
+ * @ht: previously created hash table to find key/value pairs within
+ * @key: given value to search array for
+ *
+ * Return: value associated with array element's key or NULL
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+
+	unsigned long int value;
+
+	if (!ht || !key)
+		return (NULL);
+
+	value = key_index((const unsigned char *)key, ht->size);
+
+	return (value);
+
+}
