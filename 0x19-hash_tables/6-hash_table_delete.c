6d2ed72fd37fcc6f004e89ffd0cfc20f0b1b30a5
cnov20 <128@holbertonschool.com>
2017-06-02 06:20:11 +0000

    Added nested while loop

--- 0x19-hash_tables/6-hash_table_delete.c
+++ 0x19-hash_tables/6-hash_table_delete.c
@@ -9,15 +9,24 @@
 void hash_table_delete(hash_table_t *ht)
 {
 
-	unsigned int i;
+	unsigned int index;
+	hash_node_t *temp_ptr;
 
-	for (i = 0; i < ht->size; i++)
+	index = 0;
+
+	for (index = 0; index < ht->size; index++)
 	{
-		free(ht);
-		free(ht->array);
+		temp_ptr = ht->array[index];
+		while (temp_ptr != NULL)
+		{
+			free(temp_ptr->key);
+			free(temp_ptr->value);
+			free(temp_ptr);
+			temp_ptr = temp_ptr->next;
+		}
 
 	}
 
-
-
+	free(ht);
+	free(ht->array);
 }
