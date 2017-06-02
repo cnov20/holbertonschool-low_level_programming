b427c2276aa212f2de55702c4eaa5773c9dce8c9
cnov20 <128@holbertonschool.com>
2017-06-02 07:15:14 +0000

    Fixed for Betty correction

--- 0x19-hash_tables/5-hash_table_print.c
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -10,29 +10,36 @@
 void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *temp_ptr;
-	unsigned int index, count;
+	unsigned int index, count, elements;
 
 	if (!ht || !ht->array)
 		return;
 
 	printf("{");
 	count = 0;
+	elements = 0;
 	for (index = 0; index < ht->size; index++)
 	{
 		temp_ptr = ht->array[index];
+
+		if (temp_ptr != NULL)
+			elements++;
+
 		while (temp_ptr != NULL)
 		{
 			printf("'%s': ", temp_ptr->key);
 			printf("'%s'", temp_ptr->value);
+			printf(", ");
 
-			if (count < 6)
-				printf(", ");
+			if (elements == count)
+				printf("xxx");
 
 			temp_ptr = temp_ptr->next;
 			count++;
 		}
 
 	}
+	printf("Count: %d - elements: %d", count, elements);
 	printf("}");
 	printf("\n");
 }
