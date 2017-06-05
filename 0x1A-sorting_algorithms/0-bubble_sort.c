3474a24b465f280b55e67573410f5c8d95ffe77f
cnov20 <128@holbertonschool.com>
2017-06-05 05:38:20 +0000

    Removed brackets from flag if statement

--- 0x1A-sorting_algorithms/0-bubble_sort.c
+++ 0x1A-sorting_algorithms/0-bubble_sort.c
@@ -20,18 +20,16 @@ void bubble_sort(int *array, size_t size)
 	{
 		if (array[i] > array[i + 1])
 		{
+			flag = 1;
 			temp = array[i];
 			array[i] = array[i + 1];
 			array[i + 1] = temp;
 			print_array(array, size);
-			flag = 1;
 
-			if (flag == 1)
-			{
-				i = -1;
-			}
-
+			if(flag == 1)
+			   i = -1;
 		}
 
 	}
+
 }
