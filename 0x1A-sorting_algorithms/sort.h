13f22e1b0247d5b9342d9d342b9cbdce0b202e45
cnov20 <128@holbertonschool.com>
2017-06-05 03:02:33 +0000

    Added header file

---
+++ 0x1A-sorting_algorithms/sort.h
@@ -0,0 +1,24 @@
+#ifndef SORT_H
+#define SORT_H
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+/* Print Function Prototypes */
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+/* Sort Function Prototypes */
+void bubble_sort(int *array, size_t size);
+
+#endif /* SORT_H */
