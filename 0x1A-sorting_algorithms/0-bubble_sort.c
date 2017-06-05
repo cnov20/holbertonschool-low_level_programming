a2f2833f52a02e7d276e3bce9f569f00cddda61f
cnov20 <128@holbertonschool.com>
2017-06-05 04:49:43 +0000

    Task 0 - Bubble Sort and Big O notation file

---
+++ 0x1A-sorting_algorithms/0-bubble_sort.c
@@ -0,0 +1,39 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array in ascending order
+ * @array - array of integers to be sorted
+ * @size - size of given array
+ *
+ * Return - nothing, void
+ */
+
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i;
+	unsigned int temp;
+	unsigned int flag;
+
+	flag = 0;
+	for (i = 0; i < size - 1; i++)
+	{
+/*		for (j = 0; j < size - 1; j++)
+		{ */
+		if (array[i] > array[i + 1])
+		{
+			temp = array[i];
+			array[i] = array[i + 1];
+			array[i + 1] = temp;
+			print_array(array, size);
+			flag = 1;
+
+			if (flag == 1)
+			{
+				i = -1;
+			}
+
+		}
+
+	}
+}
