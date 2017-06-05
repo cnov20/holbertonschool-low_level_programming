8821289bbe2f44e53149017f3ca6823c026739fd
cnov20 <128@holbertonschool.com>
2017-06-05 05:42:00 +0000

    Added updated header file

--- 0x1A-sorting_algorithms/sort.h
+++ 0x1A-sorting_algorithms/sort.h
@@ -22,5 +22,6 @@ void print_list(const listint_t *list);
 
 /* Sort Function Prototypes */
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
 
 #endif /* SORT_H */
