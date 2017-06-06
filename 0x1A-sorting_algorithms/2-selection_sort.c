dbf62c258f946c7479746bdedde650b205e7d70f
cnov20 <128@holbertonschool.com>
2017-06-06 15:24:42 +0000

    Implemented flag to only print if swap occurs

--- 0x1A-sorting_algorithms/2-selection_sort.c
+++ 0x1A-sorting_algorithms/2-selection_sort.c
@@ -12,27 +12,33 @@ void selection_sort(int *array, size_t size)
 {
 
 	size_t i, j;
-	int temp, index, minimum;
+	int temp, index, minimum, flag;
 
 	if (size < 2 || !array || !size)
 		return;
 
+
 	for (i = 0; i < size - 1; i++)
 	{
+		flag = 0;
 		temp = array[i];
 		minimum = array[i];
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < minimum)
 			{
+				flag = 1;
 				index = j;
 				minimum = array[j];
 			}
 
 		}
 
-		array[i] = array[index];
-		array[index] = temp;
-		print_array(array, size);
+		if (flag == 1)
+		{
+			array[i] = array[index];
+			array[index] = temp;
+			print_array(array, size);
+		}
 	}
 }
