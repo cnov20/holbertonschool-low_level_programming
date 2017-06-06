8685bbaa32feb6cb39a587876f5ddd94245cd089
cnov20 <128@holbertonschool.com>
2017-06-06 14:10:45 +0000

    Updated header comments

--- 0x1A-sorting_algorithms/sort.h
+++ 0x1A-sorting_algorithms/sort.h
@@ -8,6 +8,7 @@
  * struct listint_s - Doubly linked list node
  *
  * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
  */
 typedef struct listint_s
