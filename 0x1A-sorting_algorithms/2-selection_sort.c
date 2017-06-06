af87cbbf138ab4e288b0c9417a22cc120d40c69b
cnov20 <128@holbertonschool.com>
2017-06-06 07:14:00 +0000

    Added minimum variable to compare array values againt and fixed for Betty style

--- 0x1A-sorting_algorithms/2-selection_sort.c
+++ 0x1A-sorting_algorithms/2-selection_sort.c
@@ -11,44 +11,26 @@
 void selection_sort(int *array, size_t size)
 {
 
-        int i, j;
-        int temp;
-/*	int minimum; */
+	size_t i, j;
+	int temp;
+	int minimum;
 
-        if (size < 2 || !array || !size)
-                return;
+	if (size < 2 || !array || !size)
+		return;
 
-	/*	minimum = INT_MIN */;
-	for (i = 0; i < size; i++);
+	minimum = 0;
+	for (i = 0; i < size; i++)
 	{
-		for (j = size; j >= 0; j--)
+		for (j = size; j > 0; j--)
 		{
 			if (array[i] < array[j])
 			{
 				temp = array[j];
 				array[j] = array[i];
-			        array[i] = temp;
+				array[i] = temp;
 				print_array(array, size);
 			}
 		}
 
 	}
 }
-
-/*        for (i = 0; i < size - 1; i++)
-        {
-                for (j = 0; j < size - 1; j++)
-                {
-                        if (array[j] > array[j + 1])
-                        {
-                                temp = array[j];
-                                array[j] = array[j + 1];
-                                array[j + 1] = temp;
-				print_array(array, size);
-                        }
-
-                }
-        }
-
-}
-*/
