39e24c379e0d852addbbf45b32ef7fd0f351dba4
cnov20 <128@holbertonschool.com>
2017-06-06 14:04:49 +0000

    Adjusted i and j

--- 0x1A-sorting_algorithms/0-bubble_sort.c
+++ 0x1A-sorting_algorithms/0-bubble_sort.c
@@ -16,9 +16,9 @@ void bubble_sort(int *array, size_t size)
 	if (size < 2 || !array)
 		return;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-		for (j = 0; j < size - 1 - i; j++)
+		for (j = 0; j < (size - 1) - i; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
