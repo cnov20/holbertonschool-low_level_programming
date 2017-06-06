51d4ee3e7daedaf584a0eecde3f74f3ab7679119
cnov20 <128@holbertonschool.com>
2017-06-06 14:02:30 +0000

    Changed first iterator to size

--- 0x1A-sorting_algorithms/0-bubble_sort.c
+++ 0x1A-sorting_algorithms/0-bubble_sort.c
@@ -16,7 +16,7 @@ void bubble_sort(int *array, size_t size)
 	if (size < 2 || !array)
 		return;
 
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size - 1 - i; j++)
 		{
