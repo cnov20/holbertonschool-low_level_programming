26794f3413d7b53cf6a632235486f92f678e480d
cnov20 <128@holbertonschool.com>
2017-06-07 02:18:55 +0000

    Updated and added function prototypes for quick_sort

--- 0x1A-sorting_algorithms/sort.h
+++ 0x1A-sorting_algorithms/sort.h
@@ -28,4 +28,9 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
+/* Sort Helper Function Prototypes */
+void recursive_sort(int *array, int start, int end, size_t size);
+int partition(int *array, int start, int end, size_t size);
+void swap_values(int *array, int i, int j);
+
 #endif /* SORT_H */
