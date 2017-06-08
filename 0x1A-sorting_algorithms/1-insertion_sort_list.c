ec91a54a5c0b6ae608d3006ad50b098ae64209ba
cnov20 <128@holbertonschool.com>
2017-06-08 06:53:20 +0000

    Removed printf debug statements and corrected for Betty style syntax

--- 0x1A-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1A-sorting_algorithms/1-insertion_sort_list.c
@@ -18,36 +18,29 @@ void insertion_sort_list(listint_t **list)
 	current = *list;
 	while (current != NULL)
 	{
+		while (current->prev != NULL && current->prev->n > current->n)
+		{
+			temp = current;
+			if (current->next != NULL)
+				current->next->prev = temp->prev;
+			current->prev->next = temp->next;
 
-			while(current->prev != NULL && current->prev->n > current->n)
-			{
-				temp = current;
-				if (current->next != NULL)
-					current->next->prev = temp->prev;
-				current->prev->next = temp->next;
+			current = current->prev;
+			temp->prev = current->prev;
+			temp->next = current;
 
+			if (current->prev != NULL)
+				current->prev->next = temp;
+			current->prev = temp;
 
-				current = current->prev;
-				temp->prev = current->prev;
-				temp->next = current;
+			current = current->prev;
 
-				if (current->prev != NULL)
-					current->prev->next = temp;
-				current->prev = temp;
+			print_list(*list);
 
-				printf("this is now current: %d\n", current->n);
-				printf("this is now temp: %d\n", temp->n);
-
-				current = current->prev;
-
-				print_list(*list);
-
-			}
+		}
 
 		/*printf("hello current: %d\n", current->n);*/
-
 		current = current->next;
-
 	}
 
 }
