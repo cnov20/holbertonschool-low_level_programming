77a92bb975d23f0c9c713f0cbd82cf281ca6c4fc
cnov20 <128@holbertonschool.com>
2017-07-20 18:20:09 +0000

    Added Task 2 - insert right node - modified function prototypes in header file

---
+++ 0x1C-binary_trees/2-binary_tree_insert_right.c
@@ -0,0 +1,35 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - inserts node as right child of another node
+ * @parent: pointer to the parent of the node to be created
+ * @value: value to be inserted into the newly created node
+ *
+ * Return - pointer to new node or NULL - upon failure
+ */
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+
+        binary_tree_t *new_node;
+
+	new_node = binary_tree_node(parent, value);
+
+	if (new_node == NULL)
+		return (NULL);
+
+
+	if(parent->right != NULL)
+	{
+		new_node->right = parent->right;
+		parent->right = new_node;
+		new_node->right->parent = new_node;
+		return (new_node);
+	}
+	else
+	{
+		parent->right = new_node;
+		return (parent->right);
+	}
+
+}
