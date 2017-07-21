ff303975326c63b7f6d2729c94fcd7a38929914a
cnov20 <128@holbertonschool.com>
2017-07-21 14:27:57 +0000

    Parameter comment description

--- 0x1C-binary_trees/17-binary_tree_sibling.c
+++ 0x1C-binary_trees/17-binary_tree_sibling.c
@@ -2,9 +2,9 @@
 
 /**
  * binary_tree_sibling - finds sibling of a node
- * @tree: pointer to the node to find the sibling of
+ * @node: pointer to the node to find the sibling of
  *
- * Return - pointer to node or NULL if no sibling found
+ * Return: pointer to node or NULL if no sibling found
  */
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
@@ -13,11 +13,11 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	if (node == NULL || node->parent == NULL)
 		return (NULL);
 
-	if (node->parent->left != NULL && node->parent->right != NULL)
-		return (node->parent->right);
+	if ((node->n > node->parent->n) && (node->parent->left != NULL))
+		return (node->parent->left);
 
-	if (node->parent->right != NULL && node->parent->left != NULL)
-		return(node->parent->left);
+	if ((node->n < node->parent->n) && (node->parent->right != NULL))
+		return (node->parent->right);
 
 	return (NULL);
 }
