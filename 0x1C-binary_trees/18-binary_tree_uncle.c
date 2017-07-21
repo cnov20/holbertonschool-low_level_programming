66284df251ffc75601c771829e2d2c95fa9b1511
cnov20 <128@holbertonschool.com>
2017-07-21 14:52:23 +0000

    Added NULL check

--- 0x1C-binary_trees/18-binary_tree_uncle.c
+++ 0x1C-binary_trees/18-binary_tree_uncle.c
@@ -13,9 +13,9 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
 
-	if (node->n < node->parent->n)
+	if (node->n < node->parent->n && node->parent->parent->left != NULL)
 		return (node->parent->parent->left);
-	if (node->n > node->parent->n)
+	if (node->n > node->parent->n && node->parent->parent->right != NULL)
 		return (node->parent->parent->right);
 
 	return (NULL);
