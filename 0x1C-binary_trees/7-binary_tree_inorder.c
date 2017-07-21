88720bfbda62b044007086e317ae002241f98411
cnov20 <128@holbertonschool.com>
2017-07-21 11:10:48 +0000

    Syntax correction

--- 0x1C-binary_trees/7-binary_tree_inorder.c
+++ 0x1C-binary_trees/7-binary_tree_inorder.c
@@ -13,7 +13,7 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 	if (tree == NULL || func == NULL)
 		return;
 
-	if(tree->n)
+	if (tree->n)
 	{
 		binary_tree_inorder(tree->left, func);
 		func(tree->n);
