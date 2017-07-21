1e5f10cece52a4b0e2879f2c3893766051b41a4d
cnov20 <128@holbertonschool.com>
2017-07-21 15:09:30 +0000

    Changed to NULL conditional in main if statements

--- 0x1C-binary_trees/18-binary_tree_uncle.c
+++ 0x1C-binary_trees/18-binary_tree_uncle.c
@@ -10,12 +10,12 @@
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 
-	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+	if (node == NULL || node->parent == NULL || !node->parent->parent)
 		return (NULL);
 
-	if (node->parent->parent->left && node->parent->parent->left != node->parent)
+	if (node->parent->parent->left && node->parent->parent->left != NULL)
 		return (node->parent->parent->left);
-	if (node->parent->parent->right && node->parent->parent->right != node->parent)
+	if (node->parent->parent->right && node->parent->parent->right != NULL)
 		return (node->parent->parent->right);
 
 	return (NULL);
