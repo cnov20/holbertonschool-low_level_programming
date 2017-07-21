b34c03fae274a915a307fea91c2a7f22b68563d6
cnov20 <128@holbertonschool.com>
2017-07-21 06:40:05 +0000

    Added NULL conditional check

--- 0x1C-binary_trees/2-binary_tree_insert_right.c
+++ 0x1C-binary_trees/2-binary_tree_insert_right.c
@@ -13,11 +13,14 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 
 	binary_tree_t *new_node;
 
+
+	if (parent == NULL)
+		return (NULL);
+
 	new_node = binary_tree_node(parent, value);
 
 	if (new_node == NULL)
 		return (NULL);
-
 
 	if (parent->right != NULL)
 	{
