9d23afd67d5f23534176034c8a71162c220b9a94
cnov20 <128@holbertonschool.com>
2017-07-21 11:12:22 +0000

    Added func parameter and node value checks

--- 0x1C-binary_trees/8-binary_tree_postorder.c
+++ 0x1C-binary_trees/8-binary_tree_postorder.c
@@ -10,10 +10,13 @@
 
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree == NULL)
+	if (tree == NULL || func == NULL)
 		return;
 
-	binary_tree_postorder(tree->left, func);
-	binary_tree_postorder(tree->right, func);
-	func(tree->n);
+	if (tree->n)
+	{
+		binary_tree_postorder(tree->left, func);
+		binary_tree_postorder(tree->right, func);
+		func(tree->n);
+	}
 }
