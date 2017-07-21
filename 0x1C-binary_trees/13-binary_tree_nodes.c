e879bc7e4b11a9f865ac93040ebef47454262f3e
cnov20 <128@holbertonschool.com>
2017-07-21 11:20:35 +0000

    Removed unnecessary else statement - Condensed line lengths for readibility

--- 0x1C-binary_trees/13-binary_tree_nodes.c
+++ 0x1C-binary_trees/13-binary_tree_nodes.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_nodes - counts the nodes with at least one child in a binary tree
+ * binary_tree_nodes - counts the nodes w/ at least one child in a binary tree
  * @tree: pointer to root node of tree to count nodes of
  *
  * Return: number of nodes
@@ -10,13 +10,9 @@
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 
-
 	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 		return (0);
-	else
-	{
-		return (1 + binary_tree_nodes(tree->left)
-			+ binary_tree_nodes(tree->right));
-	}
 
+	return (1 + binary_tree_nodes(tree->left)
+		+ binary_tree_nodes(tree->right));
 }
