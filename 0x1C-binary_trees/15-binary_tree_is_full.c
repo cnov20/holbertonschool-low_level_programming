38190e81516b0c844ec19a9ed39a6335a58e3018
cnov20 <128@holbertonschool.com>
2017-07-21 01:44:58 +0000

    Added Task 15 - check if binary tree is full and updated func prototypes in header

---
+++ 0x1C-binary_trees/15-binary_tree_is_full.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_full - checks if a binary tree is full
+ * @tree: pointer to root node of tree to be checked
+ *
+ * Return - 1 if FULL - 0 if not full or tree is NULL
+ */
+
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	return (binary_tree_is_full(tree->left) == binary_tree_is_full(tree->right));
+
+}
--- 0x1C-binary_trees/binary_trees.h
+++ 0x1C-binary_trees/binary_trees.h
@@ -46,5 +46,7 @@ size_t binary_tree_depth(const binary_tree_t *node);
 size_t binary_tree_size(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
+int binary_tree_balance(const binary_tree_t *tree);
+int binary_tree_is_full(const binary_tree_t *tree);
 
 #endif /* __BINARY_TREES_H__ */
