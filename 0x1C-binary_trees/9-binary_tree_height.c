5ad7ae7a2035ce70462af9a2585350b22154131e
cnov20 <128@holbertonschool.com>
2017-07-21 12:36:29 +0000

    Changed conditional to AND for NULL on either side

--- 0x1C-binary_trees/9-binary_tree_height.c
+++ 0x1C-binary_trees/9-binary_tree_height.c
@@ -14,7 +14,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t height_left = 0;
 	size_t height_right = 0;
 
-	if (tree == NULL || tree->left == NULL || tree->right == NULL)
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 		return (0);
 
 	while (tree->left != NULL)
