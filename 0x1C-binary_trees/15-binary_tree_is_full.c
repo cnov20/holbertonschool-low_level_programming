362711fb18817a0371d799480b4ccc73c3b4f31a
cnov20 <128@holbertonschool.com>
2017-07-21 04:23:44 +0000

    Fixed Return in comments - betty style

--- 0x1C-binary_trees/15-binary_tree_is_full.c
+++ 0x1C-binary_trees/15-binary_tree_is_full.c
@@ -4,7 +4,7 @@
  * binary_tree_is_full - checks if a binary tree is full
  * @tree: pointer to root node of tree to be checked
  *
- * Return - 1 if FULL - 0 if not full or tree is NULL
+ * Return: 1 if FULL - 0 if not full or tree is NULL
  */
 
 int binary_tree_is_full(const binary_tree_t *tree)
