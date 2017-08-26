32eede5cc52ca329047b1e314f200f84b2e2fa7b
cnov20 <128@holbertonschool.com>
2017-08-26 18:51:29 +0000

    Updated print helper function prototype - take in low value, high value, and array

--- 0x1D-search_algorithms/search_algos.h
+++ 0x1D-search_algorithms/search_algos.h
@@ -10,6 +10,6 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 
 /* Helper function prototypes */
-
+void print_search(unsigned int low, unsigned int high, int *array);
 
 #endif /* SEARCH_ALGOS_H */
