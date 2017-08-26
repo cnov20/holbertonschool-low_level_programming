ec599fc2ee3418d20af839286e8b40e1ef3901a7
cnov20 <128@holbertonschool.com>
2017-08-26 03:29:28 +0000

    Added header file - Initial

---
+++ 0x1D-search_algorithms/search_algos.h
@@ -0,0 +1,16 @@
+/* Inlcude Guard - Header */
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+/* Include libraries */
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Function prototypes */
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+/* Helper function prototypes */
+
+
+#endif /* SEARCH_ALGOS_H */
