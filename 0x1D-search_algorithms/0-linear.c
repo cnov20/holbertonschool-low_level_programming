e4c257bd36149e09bf956f31fa3203bf830982d7
cnov20 <128@holbertonschool.com>
2017-08-26 12:15:31 +0000

    Corrected syantx for betty / ISO-90 - Fully working solution

--- 0x1D-search_algorithms/0-linear.c
+++ 0x1D-search_algorithms/0-linear.c
@@ -14,10 +14,10 @@ int linear_search(int *array, size_t size, int value)
 
 	unsigned int i;
 
-	if(!array || !value || ! size)
+	if (!array || !value || !size)
 		return (-1);
 
-	for(i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%u] = [%d]\n", i, array[i]);
 
